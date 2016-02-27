38548825dba061e29cc13ba411583de2fecb8232
Vinod Muthusamy <vmuthus@us.ibm.com>
2016-02-26 22:25:55 -0500

    Remove unused parameter in date action.

--- catalog/utils/date.js
+++ catalog/utils/date.js
@@ -1,7 +1,7 @@
 /**
  * Print and return the current date.
  */
-function main(params) {
+function main() {
     var now = new Date();
     console.log('It is now ' + now);
     return { date: now };
