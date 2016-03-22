7e76453529f7cf335ce28fb626c739d3411427a8
Nick M. Mitchell <nickm@us.ibm.com>
2016-03-22 14:24:58 -0400

    update installSlack.sh to make the user field bindTime and required; fix some issues with the prior installSystem.sh updates, e.g. i had count:2 rather than the correct count:3 for the sampleOutput of wordCount

--- catalog/installSystem.sh
+++ catalog/installSystem.sh
@@ -31,7 +31,7 @@ install utils/sort.js             util/sort -a description 'Sorts an array' \
                                            -a sampleInput '{ "lines": [4, 2, 3] }' \
                                            -a sampleOutput '{ "lines": [2, 3, 4], "length": 3 }'
 
-install utils/head.js             util/head -a description 'Include only prefix an array' \
+install utils/head.js             util/head -a description 'Extract prefix of an array' \
                                            -a parameters '[ { "name": "lines", "required": true, "type": "array", "description": "An array of strings" }, { "name": "num", "required": false, "type": "integer", "description": "The length of the prefix" }]' \
                                            -a sampleInput '{ "lines": [4, 2, 3], num: 2 }' \
                                            -a sampleOutput '{ "lines": [4, 2], "num": 2 }'
@@ -45,13 +45,13 @@ install samples/hello.js          samples/helloWorld -a description 'Demonstrate
                                            -a sampleLogOutput '2016-03-22T01:02:26.387624916Z stdout: hello Cat!'
 
 install samples/greeting.js       samples/greeting -a description 'Returns a friendly greeting' -a parameters '[{"name": "name", "required":false}, {"name": "place", "required":false, "description":"The string to be included in the return value"}]' \
-                                           -a sampleInput '{ "payload": "Cat", "place": "Narrowsburg" }' \
-                                           -a sampleOutput '{ "payload": "Hello, Cat from Narrowsburg!" }' \
+                                           -a sampleInput '{ "name": "Cat", "place": "Narrowsburg" }' \
+                                           -a sampleOutput '{ "name": "Hello, Cat from Narrowsburg!" }' \
                                            -a sampleLogOutput "2016-03-22T01:07:08.384982272Z stdout: params: { place: 'Narrowsburg', payload: 'Cat' }"
 
-install samples/wc.js             samples/wordCount -a description 'Count words in a string' -a parameters '[{"name": "payload", "required":false, "description":"A string"}]'\
+install samples/wc.js             samples/wordCount -a description 'Count words in a string' -a parameters '[{"name": "payload", "required":true, "description":"A string"}]'\
                                                     -a sampleInput '{ "payload": "Five fuzzy felines"}' \
-                                                    -a sampleOutput '{ "count": 2 }' \
+                                                    -a sampleOutput '{ "count": 3 }' \
                                                     -a sampleLogOutput "2016-03-22T01:10:07.361649586Z stdout: The message 'Five fuzzy felines' has 3 words"
 
 install samples/echo.js           samples/echo -a description 'Returns the input' -a parameters '[{"name": "payload", "required":false, "description": "Any JSON entity"}]' \
