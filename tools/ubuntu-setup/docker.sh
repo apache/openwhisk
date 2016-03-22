810a0d8137407ff179353f384a58ac11deaca55c
Jeremias Werner <jeremias.werner@gmail.com>
2016-03-22 16:53:04 +0100

    issue #2 support docker 1.10 in ubuntu setup

--- tools/ubuntu-setup/docker.sh
+++ tools/ubuntu-setup/docker.sh
@@ -2,26 +2,23 @@
 
 set -ex
 
-sudo apt-get -y install apt-transport-https
+sudo apt-get -y install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sudo sh -c "echo deb https://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+sudo sh -c "echo deb https://apt.dockerproject.org/repo ubuntu-trusty main  > /etc/apt/sources.list.d/docker.list"
 sudo apt-get -y update -qq
+
+sudo apt-get purge lxc-docker
+sudo apt-cache policy docker-engine
 
 # AUFS
 sudo apt-get -y install linux-image-extra-$(uname -r)
 
 # DOCKER
-sudo apt-get install -q -y --force-yes lxc-docker-1.9.0
+#sudo apt-get install -q -y --force-yes lxc-docker-1.10.3
+sudo apt-get install -y --force-yes docker-engine
 
 # enable (security - use 127.0.0.1)
-#echo 'DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --api-enable-cors"' >> /etc/default/docker
-#echo 'DOCKER_OPTS="-H tcp://127.0.0.1:4243 -H unix:///var/run/docker.sock --api-enable-cors"' >> /etc/default/docker
-#echo 'DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --api-enable-cors"' >> /etc/default/docker
-#echo 'DOCKER_OPTS="-H tcp://172.17.42.1:4243 --api-enable-cors"' >> /etc/default/docker
-#echo 'DOCKER_OPTS="-H tcp://172.17.42.1:4243 -H unix:///var/run/docker.sock --api-enable-cors"' >> /etc/default/docker
-#echo 'DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --api-enable-cors --storage-driver=aufs"' >> /etc/default/docker
 sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --api-enable-cors --storage-driver=aufs"'\'' >> /etc/default/docker'
-
 sudo gpasswd -a vagrant docker
 
 sudo service docker restart
