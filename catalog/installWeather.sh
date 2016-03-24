3b81de0aed9a09f0cd576dd8bcd5a3c1123810a5
Vatche Ishakian <vishaki@us.ibm.com>
2016-03-24 11:31:25 -0400

    adding weather pollen action
    adding annotations to the pollen action
    
    remove spaces

--- catalog/installWeather.sh
+++ catalog/installWeather.sh
@@ -23,6 +23,12 @@ install 'weather/forecast.js'       weather/forecast \
     -a sampleInput '{"latitude":"34.063", "longitude":"-84.217", "apiKey":"XXX"}' \
     -a sampleOutput '{"forecasts":[ {"dow":"Monday", "min_temp":30, "max_temp":38, "narrative":"Cloudy"} ]}'
 
+install 'weather/pollen.js'       weather/pollen \
+    -a description 'Weather.com pollen information' \
+    -a parameters '[{"name":"latitude", "required":true}, {"name":"longitude", "required":true}, {"name":"apiKey", "required":true, "type":"password", "bindTime":true}]'
+    -a sampleInput '{"latitude":"34.063", "longitude":"-84.217", "apiKey":"XXX"}' \
+	-a sampleOutput '{"pollenobservations":[{"class":"pollenobs", "loc_id":"ATL", "loc_nm":"Atlanta", "loc_st":"GA", "rpt_dt":"2016-03-29T12:26:00Z", "process_time_gmt":1396983306, "treenames":[{"tree_nm":"Oak"}], "total_pollen_cnt":1156, "pollenobservation":[{"pollen_type":"Tree","pollen_idx":"4","pollen_desc":"Very High"}]}]}'
+
 waitForAll
 
 echo Weather package ERRORS = $ERRORS
