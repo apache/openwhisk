cf6029e14434cc68fb6f8c0b327572c77c244dbb
Dominik Jall <djall@de.ibm.com>
2016-04-15 13:25:42 +0200

    pinning docker to 1.10.3 on ubuntu-setup also.

--- tools/ubuntu-setup/docker.sh
+++ tools/ubuntu-setup/docker.sh
@@ -14,8 +14,7 @@ sudo apt-cache policy docker-engine
 sudo apt-get -y install linux-image-extra-$(uname -r)
 
 # DOCKER
-#sudo apt-get install -q -y --force-yes lxc-docker-1.10.3
-sudo apt-get install -y --force-yes docker-engine
+sudo apt-get install -y --force-yes docker-engine=1.10.3-0~trusty
 
 # enable (security - use 127.0.0.1)
 sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --api-enable-cors --storage-driver=aufs"'\'' >> /etc/default/docker'
