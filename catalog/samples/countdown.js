f39b85147afafca063d69692e44709704be62696
Fatih Ulusoy <ulusoy@de.ibm.com>
2016-04-20 17:17:02 +0200

    Issue #1673. Fixing countdown action.
    Signed-off-by: Christian Bickel <cbickel@de.ibm.com>

--- catalog/samples/countdown.js
+++ catalog/samples/countdown.js
@@ -5,14 +5,22 @@
 function main(params) {
     var n = parseInt(params.n);
     console.log(n);
-    if (n == 0) {
+    if (n === 0) {
         console.log('Happy New Year!');
     } else if (n > 0) {
         whisk.invoke({
             name : 'countdown',
             parameters : {
                 n : n - 1
+            },
+            next : function(error, activation) {
+                if (!error) {
+                    whisk.done();
+                } else {
+                    whisk.error(error);
+                }
             }
-        })
+        });
+        return whisk.async();
     }
 }
