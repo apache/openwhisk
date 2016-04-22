e2546a9bfb13a099ca6ad3886fa4d553e4b7625f
Joachim Valdez <joachimvaldez@gmail.com>
2016-04-22 14:43:54 -0400

    speech to text action
    Signed-off-by: Jason Peterson <jasonpet@us.ibm.com>

--- catalog/installWatson.sh
+++ catalog/installWatson.sh
@@ -17,6 +17,102 @@ createPackage watson \
 
 waitForAll
 
+install "$CATALOG_HOME/watson/speechToText.js" \
+    watson/speechToText \
+    -a description 'Convert speech to text' \
+    -a parameters '[
+  {
+    "name": "content_type",
+    "required": true,
+    "description": "The MIME type of the audio"
+  },
+  {
+    "name": "model",
+    "required": false,
+    "description": "The identifier of the model to be used for the recognition request"
+  },
+  {
+    "name": "continuous",
+    "required": false,
+    "description": "Indicates whether multiple final results that represent consecutive phrases separated by long pauses are returned"
+  },
+  {
+    "name": "inactivity_timeout",
+    "required": false,
+    "description": "The time in seconds after which, if only silence (no speech) is detected in submitted audio, the connection is closed"
+  },
+  {
+    "name": "interim_results",
+    "required": false,
+    "description": "Indicates whether the service is to return interim results"
+  },
+  {
+    "name": "keywords",
+    "required": false,
+    "description": "A list of keywords to spot in the audio"
+  },
+  {
+    "name": "keywords_threshold",
+    "required": false,
+    "description": "A confidence value that is the lower bound for spotting a keyword"
+  },
+  {
+    "name": "max_alternatives",
+    "required": false,
+    "description": "The maximum number of alternative transcripts to be returned"
+  },
+  {
+    "name": "word_alternatives_threshold",
+    "required": false,
+    "description": "A confidence value that is the lower bound for identifying a hypothesis as a possible word alternative"
+  },
+  {
+    "name": "word_confidence",
+    "required": false,
+    "description": "Indicates whether a confidence measure in the range of 0 to 1 is to be returned for each word"
+  },
+  {
+    "name": "timestamps",
+    "required": false,
+    "description": "Indicates whether time alignment is returned for each word"
+  },
+  {
+    "name": "X-Watson-Learning-Opt-Out",
+    "required": false,
+    "description": "Indicates whether to opt out of data collection for the call"
+  },
+  {
+    "name": "watson-token",
+    "required": false,
+    "description": "Provides an authentication token for the service as an alternative to providing service credentials"
+  },
+  {
+    "name": "encoding",
+    "required": true,
+    "description": "The encoding of the speech binary data"
+  },
+  {
+    "name": "payload",
+    "required": true,
+    "description": "The encoding of the speech binary data"
+  },
+  {
+    "name": "username",
+    "required": true,
+    "bindTime": true,
+    "description": "The Watson service username"
+  },
+  {
+    "name": "password",
+    "required": true,
+    "type": "password",
+    "bindTime": true,
+    "description": "The Watson service password"
+  }
+]' \
+    -a sampleInput '{"payload":"<base64 encoding of a wav file>", "encoding":"base64", "content_type":"audio/wav", "username":"XXX", "password":"XXX"}' \
+    -a sampleOutput '{"data":"Hello."}'
+
 install "$CATALOG_HOME/watson/translate.js" \
     watson/translate \
     -a description 'Translate text' \
