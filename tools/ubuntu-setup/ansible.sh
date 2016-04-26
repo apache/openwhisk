6a387e52ac05f94d8c5616c7a6e072663f200f90
Dominik Jall <djall@de.ibm.com>
2016-04-26 17:30:49 +0200

    install ansible via pip. pin version to 2.0.2.0

--- tools/ubuntu-setup/ansible.sh
+++ tools/ubuntu-setup/ansible.sh
@@ -1,7 +1,7 @@
-sudo apt-get install -y software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update -y
-sudo apt-get install -y ansible=2.0.1.0-1ppa~trusty
+
+sudo pip install ansible==2.0.2.0
+sudo pip install docker-py
+
 
 ansible --version
 ansible-playbook --version
