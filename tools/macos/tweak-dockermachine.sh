5c22d75e587b15f82a6d6ee871a0f437921cc5ce
Dominik Jall <djall@de.ibm.com>
2016-04-26 09:39:11 +0200

    issue #208 introduce new environment for mac. fix prereqs
    issue #208 update with instructions for mac users
    
    issue #208 install pip only if it is not already installed.
    
    issue #208 update README with fixes. also fix tweak-dockermachine.sh with ansible prereqs. genHosts.sh now uses 'whisk' as default machine name.
    
    issue #208: setup mac using ansible means instead of script. playbooks for pre-deploy and post-deploy steps.
    
    issue #208: make couchdb as db default for mac
    
    issue #208 fine-tune clean of invoker containers. try to remove only user actions.
    
    issue #208 rework README.md. also add support to specify docker-machine name in tweak script.
    
    issue #208 add image tag in hotswap readme section.
    
    issue #208 incorporate code review feedback. make local the default env. make couch the default db.
    
    issue #208 add readme for default env
    
    issue #208 fix minor issues with sftp vs scp on mac. also mention that image tags are optional for hotswap.

--- tools/macos/tweak-dockermachine.sh
+++ tools/macos/tweak-dockermachine.sh
@@ -3,24 +3,38 @@
 # > docker-machine create whisk --driver virtualbox
 
 # Set this to the name of the docker-machine VM.
-MACHINE_NAME=whisk
+MACHINE_NAME=${1:-whisk}
 
 # Disable TLS.
 docker-machine ssh $MACHINE_NAME "echo DOCKER_TLS=no |sudo tee -a /var/lib/boot2docker/profile > /dev/null"
-docker-machine ssh $MACHINE_NAME "echo DOCKER_HOST=\'-H tcp://0.0.0.0:2375\' |sudo tee -a /var/lib/boot2docker/profile > /dev/null"
+docker-machine ssh $MACHINE_NAME "echo DOCKER_HOST=\'-H tcp://0.0.0.0:4243\' |sudo tee -a /var/lib/boot2docker/profile > /dev/null"
 
-# Forward standard docker port.
-docker-machine ssh $MACHINE_NAME "echo sudo iptables -t nat -A PREROUTING -p tcp --dport 4243 -j REDIRECT --to-port 2375 |sudo tee -a /var/lib/boot2docker/profile > /dev/null"
+# Perist prereqs
+cat <<WHISK_LOAD_EOF > bootsync.sh
+#!/bin/sh
+/sbin/syslogd
+su - docker -c "tce-load -wi python"
+WHISK_LOAD_EOF
 
-# Run syslogd.
-docker-machine ssh $MACHINE_NAME "echo /sbin/syslogd |sudo tee /var/lib/boot2docker/bootsync.sh > /dev/null"
+docker-machine scp bootsync.sh "$MACHINE_NAME:/tmp"
+docker-machine ssh $MACHINE_NAME "sudo mv /tmp/bootsync.sh /var/lib/boot2docker/"
 docker-machine ssh $MACHINE_NAME "sudo chmod +x /var/lib/boot2docker/bootsync.sh"
+
+rm -f bootsync.sh
+
+
+# Install prereqs
+docker-machine ssh $MACHINE_NAME "sudo /var/lib/boot2docker/bootsync.sh > /dev/null"
+
+# Restart docker daemon.
+docker-machine ssh $MACHINE_NAME "sudo /etc/init.d/docker restart"
 
 # Set routes on host.
 # If you notice the route forwarding is immediately deleted after this script
 # runs (you can check by running 'route monitor') then shut down your networking
 # (turn off wifi and disconnet all networking cables), wait a few secs and try
 # again; you should see the route stick and now you can reenable networking.
+echo "Adding route forwarding on your host machine, enter sudo password if/when prompted"
 MACHINE_VM_IP=$(docker-machine ip $MACHINE_NAME)
 sudo route -n -q delete 172.17.0.0/16
 sudo route -n -q add 172.17.0.0/16 $MACHINE_VM_IP
@@ -29,9 +43,8 @@ netstat -nr |grep 172\.17
 # Env variables to set.
 # Note, the user CAN NOT do eval $(docker-machine env whisk) because of a bug in docker-machine that assumes TLS even if not enabled
 echo Run the following:
-echo "  " export DOCKER_HOST="tcp://$MACHINE_VM_IP:2375"
+echo "  " export DOCKER_HOST="tcp://$MACHINE_VM_IP:4243"
 
-# Restart docker VM.
-docker-machine restart $MACHINE_NAME
+
 
 
