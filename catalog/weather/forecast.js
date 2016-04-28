1f49ee2e5cfe67d6b8593df5604caa47bdeb9e94
NICHOLAS W. SPEETER <nwspeete@us.ibm.com>
2016-04-28 15:13:59 -0400

    Modifying the weather package to use IBM Weather Insights. Removed pollen.js as IBM Weather Insights does not yet have a pollen API.

--- catalog/weather/forecast.js
+++ catalog/weather/forecast.js
@@ -12,28 +12,19 @@ var request = require('request');
  * @return The hourly forecast for the lat/long.
  */
 function main(params) {
-    console.log('input params:', params)
+    console.log('input params:', params);
     var apiKey = params.apiKey;
     var lat = params.latitude || '0';
     var lon = params.longitude ||  '0';
-    var zip = params.zipCode;
-    var language = 'en-US';
-    var units = 'm';
+    var language = params.language || 'en-US';
+    var units = params.units || 'm';
 
     // Construct url.
-    var url = 'https://api.weather.com/v1';
-    if (zip)
-        url += '/location/' + zip + ':4:US';
-    else
-        url += '/geocode/' + lat + '/' + lon;
-    url += '/forecast/daily/10day.json'
-         + '?language=' + language
-         + '&apiKey=' + apiKey
-         + '&units=' + units;
+    var url = 'https://' + apiKey + '@twcservice.mybluemix.net:443/api/weather/v2/forecast/daily/10day?units=' + units + '&geocode=' + lat + '%2C' + lon + '&language=' + language;
 
     console.log('url:', url);
     request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
+        if (!error && response.statusCode === 200) {
             var j = JSON.parse(body);
             whisk.done(j);
         } else {
