a9bc71703763d5b05b9904cd5e3e21b3f34771c3
Carlos Santana <csantana@us.ibm.com>
2016-05-03 18:20:01 -0400

    Fix #309 added dependency for ansible install using pip

--- tools/ubuntu-setup/ansible.sh
+++ tools/ubuntu-setup/ansible.sh
@@ -1,7 +1,15 @@
 
-sudo pip install ansible==2.0.2.0
+sudo apt-get -y install python-dev 
+sudo apt-get -y install libffi-dev
+sudo apt-get -y install libssl-dev
+sudo pip install paramiko 
+sudo pip install PyYAML 
+sudo pip install Jinja2 
+sudo pip install httplib2 
+sudo pip install six 
+sudo pip install markupsafe  
 sudo pip install docker-py
-
+sudo pip install ansible==2.0.2.0
 
 ansible --version
 ansible-playbook --version
