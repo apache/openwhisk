89e3c1159d3919e672af3fac258c25a657a9c3db
Paul Castro <pcastro@Pauls-MacBook-Pro.local>
2016-05-04 14:06:28 -0400

    #289 Add Kitura and Dispatch test to Swift3 tests

--- tests/src/actionContainers/Swift3ActionContainerTests.scala
+++ tests/src/actionContainers/Swift3ActionContainerTests.scala
@@ -70,6 +70,58 @@ class Swift3ActionContainerTests extends FlatSpec
         err.trim shouldBe empty
     }
 
+    it should "properly use KituraNet and Dispatch" in {
+        val (out, err) = withSwiftContainer { c =>
+            val code = """
+                | import Foundation
+                | import Dispatch
+                | import KituraNet
+                | func main(args: [String: Any]) -> [String: Any] {
+                |   var str = "No response"
+                |   let url = args["getUrl"] as? String
+                |   dispatch_sync(dispatch_get_global_queue(0,0)) {
+                |       Http.get(url!) { response in
+                |           do {
+                |               if let response = response {
+                |                   str = try response.readString()!
+                |               }
+                |           } catch {
+                |               print("Error reading server response: \(error)")
+                |           }
+                |       }
+                |   }
+                |   var result: [String:Any]?
+                |   let data = str.bridge().dataUsingEncoding(NSUTF8StringEncoding)!
+                |   do {
+                |       result = try NSJSONSerialization.jsonObject(with: data, options:[]) as? [String:Any]
+                |   } catch {
+                |      print("Error serializing server response: \(error)")
+                |   }
+                |   if let result = result {
+                |       return result
+                |   }
+                |   return ["message":str]
+                | }
+            """.stripMargin
+
+            val (initCode, _) = c.init(initPayload(code))
+
+            initCode should be(200)
+
+            val argss = List(
+                JsObject("getUrl" -> JsString("https://httpbin.org/get")))
+
+            for (args <- argss) {
+                val (runCode, out) = c.run(runPayload(args))
+                runCode should be(200)
+            }
+        }
+
+        // note: "out" will likely not be empty as swift build likes
+        // to print status messages and there doesn't seem to be a way to quiet them
+        err.trim shouldBe empty
+    }
+
     it should "return some error on action error" in {
         withSwiftContainer { c =>
             val code = """
