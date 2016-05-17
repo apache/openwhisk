0c553ad169d66690f52a8420ab692ee0eea44731
Justin Berstler <bjustin@us.ibm.com>
2016-05-17 13:23:01 -0400

    Freeze Kitura-net at 0.13.8
    This specific version is needed in order to optionally disable SSL verification.

--- core/swift3Action/spm-build/Package.swift
+++ core/swift3Action/spm-build/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "Action",
         dependencies: [
-            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", majorVersion: 0, minor: 13),
+            .Package(url: "https://github.com/IBM-Swift/Kitura-net.git", "0.13.8"),
             .Package(url: "https://github.com/IBM-Swift/SwiftyJSON.git", majorVersion: 7)
         ]
 )
