9fe18779491246a38d291d26199ac70483336650
Perry Cheng <perry@us.ibm.com>
2016-05-23 14:14:54 -0400

    Increase wait time for log sentinel marker to 1.5 seconds.
    Ignore CLI Swift3 test temporarily.

--- core/dispatcher/src/whisk/core/invoker/Invoker.scala
+++ core/dispatcher/src/whisk/core/invoker/Invoker.scala
@@ -289,7 +289,7 @@ class Invoker(
 
     // The nodeJsAction runner inserts this line in the logs at the end
     // of each activation
-    private val LogRetryCount = 10
+    private val LogRetryCount = 15
     private val LogRetry = 100 // millis
     private val LOG_ACTIVATION_SENTINEL = "XXX_THE_END_OF_A_WHISK_ACTIVATION_XXX"
     private val nodejsImageName = WhiskAction.containerImageName(NodeJSExec("", None), config.dockerRegistry, config.dockerImageTag)
