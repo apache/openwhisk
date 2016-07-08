e5209716eb1c4b475d2c331eb24aa13288933911
Markus Thoemmes <markus.thoemmes@de.ibm.com>
2016-07-09 01:10:33 +0200

    Further post-ant cleanup
    This removes duplication introduced in the ant -> ansible migration.

--- services/consul/startconsulserver.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#start consul
-/bin/consul agent -config-file=/consul/config/server-config.json -data-dir=/consul/data  >> /logs/consul.log 2>&1 
--- tools/ubuntu-setup/emacs.sh
+++
@@ -1,1 +0,0 @@
-sudo apt-get -y install emacs
