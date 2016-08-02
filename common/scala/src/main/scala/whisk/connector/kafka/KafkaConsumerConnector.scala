b7f52a73bb5c0abda6cb1931b1f8cce712a25d60
Christian Bickel <cbickel@de.ibm.com>
2016-08-02 15:27:10 +0200

    Prevent invoker from not fetching messages anymore after exceptions on commit and increase session timeout #2306
    Signed-off-by: Jeremias Werner <JEREWERN@de.ibm.com>

--- common/scala/src/main/scala/whisk/connector/kafka/KafkaConsumerConnector.scala
+++ common/scala/src/main/scala/whisk/connector/kafka/KafkaConsumerConnector.scala
@@ -90,7 +90,7 @@ class KafkaConsumerConnector(
         val props = new Properties
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupid)
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkahost)
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 10000.toString)
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 30000.toString)
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true.toString)
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, 10000.toString)
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, maxPeek.toString)
