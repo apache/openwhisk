2e28fa3e71a1386203b8e36ac8d822fa9f928ece
Ioana Baldini <ioana@us.ibm.com>
2016-08-18 13:26:49 -0400

    wip remove unused import

--- core/controller/src/main/scala/whisk/core/entitlement/ActionCollection.scala
+++ core/controller/src/main/scala/whisk/core/entitlement/ActionCollection.scala
@@ -18,20 +18,13 @@ package whisk.core.entitlement
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
-import spray.http.HttpMethod
-import spray.http.HttpMethods.DELETE
-import spray.http.HttpMethods.GET
-import spray.http.HttpMethods.POST
-import spray.http.HttpMethods.PUT
 import whisk.common.TransactionId
 import whisk.core.entity.DocId
 import whisk.core.entity.SequenceExec
-import whisk.core.entity.Subject
 import whisk.core.entity.WhiskAction
 import whisk.core.entity.WhiskPackage
 import whisk.core.entity.types.EntityStore
-import scala.concurrent.Promise
-import scala.util.{Failure, Success, Try}
+import scala.util.{Failure, Success}
 import whisk.core.entity.WhiskEntity
 import whisk.core.entity.EntityName
 import whisk.core.entity.Namespace
