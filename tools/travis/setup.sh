f031bb2f44b150b59db5a40cccbaab7d423d7dcd
Dominik Jall <djall@de.ibm.com>
2016-09-08 11:35:07 +0200

    bump travis docker to 1.12.0
    Signed-off-by: Xin Cai <xincai@de.ibm.com>
    
    add service docker restart
    
    add docker info statements

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 sudo gpasswd -a travis docker
-sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --api-enable-cors --storage-driver=aufs"'\'' > /etc/default/docker'
+sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --storage-driver=overlay"'\'' > /etc/default/docker'
 
 # Docker
 sudo apt-get -y update -qq
-sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y install docker-engine=1.9.1-0~trusty
-docker --version
+sudo apt-get -o Dpkg::Options::="--force-confold" --force-yes -y install docker-engine=1.12.0-0~trusty
+sudo service docker restart
+echo "Docker Version:"
+docker version
+echo "Docker Info:"
+docker info
 
 # Python
 sudo apt-get -y install python-pip
