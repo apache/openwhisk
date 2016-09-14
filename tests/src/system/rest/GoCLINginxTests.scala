cce3e8c9571ac7fe7ae6e275d03cc2a668b17c7e
dubeejw <jwdubee@us.ibm.com>
2016-09-14 18:01:16 -0400

    Do Not Cross Compile CLI by Default
    - Use build argument to determine whether to cross-compile the CLI
    - Use build argument to determine whether or not to run cross-compiled CLI tests

--- tests/src/system/rest/GoCLINginxTests.scala
+++ tests/src/system/rest/GoCLINginxTests.scala
@@ -25,12 +25,12 @@ import com.jayway.restassured.RestAssured
 
 import spray.json._
 import DefaultJsonProtocol._
+
 /**
  * Basic tests of the download link for Go CLI binaries
  */
 @RunWith(classOf[JUnitRunner])
 class GoCLINginxTests extends FlatSpec with Matchers with RestUtil {
-
     val ProductName = "OpenWhisk_CLI"
     val DownloadLinkGoCli = "cli/go/download"
     val OperatingSystems = List("mac", "linux", "windows")
@@ -38,84 +38,45 @@ class GoCLINginxTests extends FlatSpec with Matchers with RestUtil {
     val ServiceURL = getServiceURL()
 
     it should s"respond to all files in root directory" in {
-        val response = RestAssured.given().config(sslconfig).
-            get(s"$ServiceURL/$DownloadLinkGoCli")
+        val response = RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli")
         response.statusCode should be(200)
         val responseString = response.body.asString
-        for (os <- OperatingSystems) {
-            responseString.contains(s"""<a href="$os/">$os/</a>""") should be(true)
+        responseString should include("""<a href="content.json">content.json</a>""")
+        val responseJSON = RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli/content.json")
+        responseJSON.statusCode should be(200)
+        val cli = responseJSON.body.asString.parseJson.asJsObject.fields("cli").convertTo[Map[String, Map[String,
+          Map[String, String]]]]
+        cli.foreach {
+            case(os, arch) => responseString should include(s"""<a href="$os/">$os/</a>""")
         }
-        responseString.contains("""<a href="content.json">content.json</a>""") should be(true)
     }
 
     it should "respond to all operating systems and architectures in HTML index" in {
-        for (os <- OperatingSystems) {
-            val response = RestAssured.given().config(sslconfig).
-              get(s"$ServiceURL/$DownloadLinkGoCli/$os")
-            response.statusCode should be(200)
-            val responseString = response.body.asString
-            for (arch <- Architectures) {
-                responseString.contains(s"""<a href="$arch/">$arch/</a>""") should be(true)
-            }
+        val responseJSON = RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli/content.json")
+        responseJSON.statusCode should be(200)
+        val cli = responseJSON.body.asString.parseJson.asJsObject.fields("cli").convertTo[Map[String, Map[String,
+          Map[String, String]]]]
+        cli.foreach {
+            case(os, arch) =>
+                val response = RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli/$os")
+                response.statusCode should be(200)
+                val responseString = response.body.asString
+                arch.foreach {
+                    case (arch, path) =>
+                        if (arch != "default") {
+                            responseString should include(s"""<a href="$arch/">$arch/</a>""")
+                        }
+                }
         }
     }
 
     it should "respond to the download paths in content.json" in {
-        val response = RestAssured.given().config(sslconfig).
-          get(s"$ServiceURL/$DownloadLinkGoCli/content.json")
+        val response = RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli/content.json")
         response.statusCode should be(200)
-        val jsObj = response.body.asString.parseJson.asJsObject.fields("cli").asJsObject
-        jsObj shouldBe getExpectedJSONIndex
-        val urls = Seq(
-            jsObj.fields("mac").asJsObject.fields("386").asJsObject.fields("path").convertTo[String],
-            jsObj.fields("mac").asJsObject.fields("default").asJsObject.fields("path").convertTo[String],
-            jsObj.fields("mac").asJsObject.fields("amd64").asJsObject.fields("path").convertTo[String],
-            jsObj.fields("linux").asJsObject.fields("386").asJsObject.fields("path").convertTo[String],
-            jsObj.fields("linux").asJsObject.fields("default").asJsObject.fields("path").convertTo[String],
-            jsObj.fields("linux").asJsObject.fields("amd64").asJsObject.fields("path").convertTo[String],
-            jsObj.fields("windows").asJsObject.fields("386").asJsObject.fields("path").convertTo[String],
-            jsObj.fields("windows").asJsObject.fields("default").asJsObject.fields("path").convertTo[String],
-            jsObj.fields("windows").asJsObject.fields("amd64").asJsObject.fields("path").convertTo[String]
-        )
-        for (url <- urls) {
-            RestAssured.given().config(sslconfig).
-              get(s"$ServiceURL/$DownloadLinkGoCli/$url").statusCode should be(200)
+        val cli = response.body.asString.parseJson.asJsObject.fields("cli").convertTo[Map[String, Map[String,
+          Map[String, String]]]]
+        cli.values.flatMap(_.values).flatMap(_.values).foreach { path =>
+            RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli/$path").statusCode should be(200)
         }
     }
-
-    def getExpectedJSONIndex = JsObject(
-        "mac" -> JsObject(
-            "386" -> JsObject(
-                "path" -> JsString("mac/386/OpenWhisk_CLI-mac-32bit.zip")
-            ),
-            "default" -> JsObject(
-                "path" -> JsString("mac/amd64/OpenWhisk_CLI-mac.zip")
-            ),
-            "amd64" -> JsObject(
-                "path" -> JsString("mac/amd64/OpenWhisk_CLI-mac.zip")
-            )
-        ),
-        "linux" -> JsObject(
-            "386" -> JsObject(
-                "path" -> JsString("linux/386/OpenWhisk_CLI-linux-32bit.tgz")
-            ),
-            "default" -> JsObject(
-                "path" -> JsString("linux/amd64/OpenWhisk_CLI-linux.tgz")
-            ),
-            "amd64" -> JsObject(
-                "path" -> JsString("linux/amd64/OpenWhisk_CLI-linux.tgz")
-            )
-        ),
-        "windows" -> JsObject(
-            "386" -> JsObject(
-                "path" -> JsString("windows/386/OpenWhisk_CLI-windows-32bit.zip")
-            ),
-            "default" -> JsObject(
-                "path" -> JsString("windows/amd64/OpenWhisk_CLI-windows.zip")
-            ),
-            "amd64" -> JsObject(
-                "path" -> JsString("windows/amd64/OpenWhisk_CLI-windows.zip")
-            )
-        )
-    )
 }
