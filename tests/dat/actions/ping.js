f2c090c496f21afaa1872a8cca47d69656c8a1fd
Jason Peterson <jasonpet@us.ibm.com>
2016-09-15 09:58:03 -0400

    updating test actions to use promises

--- tests/dat/actions/ping.js
+++ tests/dat/actions/ping.js
@@ -3,23 +3,25 @@ function main(msg) {
     console.log('Pinging to ' + hostToPing);
 
     var spawn = require('child_process').exec;
-    var child = spawn('ping -c 3 ' + hostToPing);
+    var promise = new Promise(function(resolve, reject) {
+        var child = spawn('ping -c 3 ' + hostToPing);
 
-    var tmp = { stdout : "", stderr : ""};
+        var tmp = {stdout: "", stderr: ""};
 
-    child.stdout.on('data', function (data) {
-        tmp.stdout = tmp.stdout + data;
+        child.stdout.on('data', function (data) {
+            tmp.stdout = tmp.stdout + data;
+        });
+
+        child.stderr.on('data', function (data) {
+            tmp.stderr = tmp.stderr + data;
+        });
+
+        child.on('close', function () {
+            console.log('stdout', tmp.stdout);
+            console.log('stderr', tmp.stderr);
+            resolve(tmp);
+        });
     });
 
-    child.stderr.on('data', function (data) {
-        tmp.stderr = tmp.stderr + data;
-    });
-
-    child.on('close', function() {
-        console.log('stdout', tmp.stdout);
-        console.log('stderr', tmp.stderr);
-        whisk.done(tmp);
-    });
-
-    return whisk.async();
+    return promise;
 }
--- tests/dat/actions/wcbin.js
+++ tests/dat/actions/wcbin.js
@@ -5,25 +5,19 @@
 function main(params) {
     var str = params.payload;
     console.log("The payload is '" + str + "'");
-    whisk.invoke({
-        name : 'wc',
-        parameters : {
-            payload : str
+
+    return whisk.invoke({
+        name: 'wc',
+        parameters: {
+            payload: str
         },
-        blocking : true,
-        next : function(error, activation) {
-            console.log('activation:', activation);
-            if (!error) {
-                var wordsInDecimal = activation.result.count;
-                var wordsInBinary = wordsInDecimal.toString(2) + ' (base 2)';
-                whisk.done({
-                    binaryCount : wordsInBinary
-                });
-            } else {
-                console.log('error:', error);
-                whisk.error(error);
-            }
-        }
+        blocking: true
+    }).then(function (activation) {
+        console.log('activation:', activation);
+        var wordsInDecimal = activation.result.count;
+        var wordsInBinary = wordsInDecimal.toString(2) + ' (base 2)';
+        return {
+            binaryCount: wordsInBinary
+        };
     });
-    return whisk.async();
 }
