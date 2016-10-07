6100ea2206c7580ac2b876467925c59c592dbfac
Markus Thoemmes <markus.thoemmes@de.ibm.com>
2016-10-07 12:51:30 +0200

    Bump ansible version to 2.1.2.0 and docker-py to 1.9.0
    - Documentation updates
    - Fix wrong syntax to pull invoker images

--- tools/macos/tweak-dockermachine.sh
+++ tools/macos/tweak-dockermachine.sh
@@ -23,7 +23,7 @@ fi
 su - docker -c \"tce-load -wi python\"
 if ! [ -x /usr/local/bin/pip ]; then
     curl -k https://bootstrap.pypa.io/get-pip.py | sudo python
-    sudo pip install \"docker-py==1.7.2\"
+    sudo pip install \"docker-py==1.9.0\"
     sudo pip install \"httplib2==0.9.2\"
 fi
 ' | sudo tee /var/lib/boot2docker/bootsync.sh > /dev/null"
--- tools/ubuntu-setup/ansible.sh
+++ tools/ubuntu-setup/ansible.sh
@@ -2,8 +2,8 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo pip install markupsafe
-sudo pip install ansible==2.0.2.0
-sudo pip install docker-py==1.7.2  # >= 1.8.0 requires docker 1.10 on the host
+sudo pip install ansible==2.1.2.0
+sudo pip install docker-py==1.9.0
 
 ansible --version
 ansible-playbook --version
