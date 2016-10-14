2bcbf1ac80c7a6dc4db9dd3ddcd694852e27a0ff
Dragos Dascalita Haut <ddragosd@gmail.com>
2016-10-13 20:11:45 -0700

    docker-composer should be written with sudo

--- tools/travis/setup-docker-compose.sh
+++ tools/travis/setup-docker-compose.sh
@@ -10,7 +10,7 @@ if [ "${version_exists}" == "false" ]
 then
     echo "Installing Docker Compose ${DOCKER_COMPOSE_VERSION}"
     sudo rm /usr/local/bin/docker-compose
-    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
 fi
 echo "Docker Compose Version:" `docker-compose --version`
