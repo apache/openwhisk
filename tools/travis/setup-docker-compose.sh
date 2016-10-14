0df7fe8ee4a630425a479d141077df838b48cc42
Dragos Dascalita Haut <ddragosd@gmail.com>
2016-10-13 20:18:19 -0700

    another try to install docker-compose

--- tools/travis/setup-docker-compose.sh
+++ tools/travis/setup-docker-compose.sh
@@ -10,7 +10,8 @@ if [ "${version_exists}" == "false" ]
 then
     echo "Installing Docker Compose ${DOCKER_COMPOSE_VERSION}"
     sudo rm /usr/local/bin/docker-compose
-    sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-    sudo chmod +x /usr/local/bin/docker-compose
+    curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` > docker-compose
+    chmod +x docker-compose
+    sudo mv docker-compose /usr/local/bin
 fi
 echo "Docker Compose Version:" `docker-compose --version`
