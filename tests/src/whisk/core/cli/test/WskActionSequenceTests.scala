a1ade38cbd671e49108e58c98cbf26fcb31dbcda
Ioana Baldini <ioana@us.ibm.com>
2016-11-08 10:35:26 -0500

    Refactor sequence tests. Leave creation and retrieval in core cli, move sequence execution testing to system basictesting

--- tests/src/whisk/core/cli/test/WskActionSequenceTests.scala
+++ tests/src/whisk/core/cli/test/WskActionSequenceTests.scala
@@ -16,11 +16,6 @@
 
 package whisk.core.cli.test
 
-import java.util.Date
-
-import scala.concurrent.duration.DurationInt
-import scala.language.postfixOps
-
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
@@ -31,13 +26,11 @@ import common.Wsk
 import common.WskAdmin
 import common.WskProps
 import common.WskTestHelpers
-import spray.json.DefaultJsonProtocol.IntJsonFormat
-import spray.json.DefaultJsonProtocol.StringJsonFormat
-import spray.json.DefaultJsonProtocol.arrayFormat
-import spray.json.JsArray
-import spray.json.JsString
-import spray.json.pimpAny
+import spray.json._
 
+/**
+ * Tests creation and retrieval of a sequence action
+ */
 @RunWith(classOf[JUnitRunner])
 class WskActionSequenceTests
     extends TestHelpers
@@ -46,50 +39,10 @@ class WskActionSequenceTests
 
     implicit val wskprops = WskProps()
     val wsk = new Wsk
-    val allowedActionDuration = 120 seconds
     val defaultNamespace = wskprops.namespace
     val user = WskAdmin.getUser(wskprops.authKey)
 
     behavior of "Wsk Action Sequence"
-
-    it should "invoke a blocking action and get only the result" in withAssetCleaner(wskprops) {
-        (wp, assetHelper) =>
-            val name = "sequence"
-            val actions = Seq("split", "sort", "head", "cat")
-            for (actionName <- actions) {
-                val file = TestUtils.getTestActionFilename(s"$actionName.js")
-                assetHelper.withCleaner(wsk.action, actionName) { (action, _) =>
-                    action.create(name = actionName, artifact = Some(file))
-                }
-            }
-
-            assetHelper.withCleaner(wsk.action, name) {
-                val sequence = actions.mkString (",")
-                (action, _) => action.create(name, Some(sequence), kind = Some("sequence"), timeout = Some(allowedActionDuration))
-            }
-
-            val now = "it is now " + new Date()
-            val args = Array("what time is it?", now)
-            val run = wsk.action.invoke(name, Map("payload" -> args.mkString("\n").toJson))
-            withActivation(wsk.activation, run, totalWait = allowedActionDuration) {
-                activation =>
-                    val result = activation.response.result.get
-                    result.fields.get("payload") shouldBe defined
-                    result.fields.get("length") should not be defined
-                    result.fields.get("lines") shouldBe Some(JsArray(Vector(now.toJson)))
-            }
-
-            // update action sequence
-            val newSequence = Seq("split", "sort").mkString (",")
-            wsk.action.create(name, Some(newSequence), kind = Some("sequence"), timeout = Some(allowedActionDuration), update = true)
-            val secondrun = wsk.action.invoke(name, Map("payload" -> args.mkString("\n").toJson))
-            withActivation(wsk.activation, secondrun, totalWait = allowedActionDuration) {
-                activation =>
-                    val result = activation.response.result.get
-                    result.fields.get("length") shouldBe Some(2.toJson)
-                    result.fields.get("lines") shouldBe Some(args.sortWith(_.compareTo(_) < 0).toArray.toJson)
-            }
-    }
 
     it should "create, and get an action sequence" in withAssetCleaner(wskprops) {
         (wp, assetHelper) =>
