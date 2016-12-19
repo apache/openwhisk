9077aef10fe822bc67805453f83224e8a790e8ce
Ioana Baldini <ioana@us.ibm.com>
2016-12-19 11:26:29 -0500

    Print exception that may have occurred during test execution before deleting assets created during test

--- tests/src/common/WskTestHelpers.scala
+++ tests/src/common/WskTestHelpers.scala
@@ -80,6 +80,11 @@ trait WskTestHelpers extends Matchers {
 
         try {
             test(wskprops, new AssetCleaner(assetsToDeleteAfterTest, wskprops))
+        } catch {
+            case t: Throwable =>
+                // log the exception that occurred in the test
+                println("Exception occurred during test execution:")
+                println(t)
         } finally {
             // delete assets in reverse order so that was created last is deleted first
             val deletedAll = assetsToDeleteAfterTest.reverse map {
