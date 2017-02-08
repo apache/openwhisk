3d9f172bf3a4f3c766c1cc010e51f5ab59ef7f64
Markus Thoemmes <markus.thoemmes@de.ibm.com>
2017-02-08 14:08:21 +0100

    use shell and measure exact time

--- core/swiftAction/swiftrunner.py
+++ core/swiftAction/swiftrunner.py
@@ -26,7 +26,7 @@ from actionproxy import ActionRunner, main, setRunner
 SRC_EPILOGUE_FILE = "./epilogue.swift"
 DEST_SCRIPT_FILE = "/swiftAction/action.swift"
 DEST_BIN_FILE = "/swiftAction/action"
-BUILD_PROCESS = [ "swiftc", "-g", "-Xfrontend", "-debug-time-function-bodies", DEST_SCRIPT_FILE, "-o", DEST_BIN_FILE ]
+BUILD_PROCESS = ["time", "swiftc", "-g", "-Xfrontend", "-debug-time-function-bodies", DEST_SCRIPT_FILE, "-o", DEST_BIN_FILE ]
 
 class SwiftRunner(ActionRunner):
 
@@ -47,7 +47,7 @@ class SwiftRunner(ActionRunner):
     def build(self):
         print "%s Start compilation" % str(datetime.datetime.now().time())
         print BUILD_PROCESS
-        p = subprocess.Popen(BUILD_PROCESS, stdout=PIPE, stderr=PIPE)
+        p = subprocess.Popen(" ".join(BUILD_PROCESS), executable="/bin/bash", stdout=PIPE, stderr=PIPE, shell=True)
         (o, e) = p.communicate()
 
         print o
