2caa240a3bed9807df7f83b7544aa5039a3cbafa
dubeejw <jwdubee@us.ibm.com>
2017-03-02 16:48:44 -0500

    Update User-facing Error Messages

--- common/scala/src/main/scala/whisk/http/ErrorResponse.scala
+++ common/scala/src/main/scala/whisk/http/ErrorResponse.scala
@@ -21,6 +21,7 @@ import scala.concurrent.duration.FiniteDuration
 import scala.util.Try
 
 import spray.http.MediaType
+import spray.http.MediaTypes
 import spray.http.StatusCode
 import spray.http.StatusCodes.Forbidden
 import spray.http.StatusCodes.NotFound
@@ -114,8 +115,9 @@ object Messages {
     /** Error for meta api. */
     val propertyNotFound = "Response does not include requested property."
     def invalidMedia(m: MediaType) = s"Response is not valid '${m.value}'."
-    val contentTypeExtentionNotSupported = """Content type must be specified and one of ["json", "html", "http", "text"]."""
-    val contentTypeNotSupported = "Content type must be appication/json or application/x-www-form-urlencoded."
+    val contentTypeExtentionNotSupported = """Extension must be specified and one of [".json", ".html", ".http", ".text"]."""
+    val contentTypeNotSupported = s"Content-type must be ${MediaTypes.`application/json`} or ${MediaTypes.`application/x-www-form-urlencoded`}."
+
     val responseNotReady = "Response not yet ready."
     val httpUnknownContentType = "Response did not specify a known content-type."
     val httpContentTypeError = "Response type in header did not match generated content type."
