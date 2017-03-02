f154f897799b61d7c27707aeb663041729b4184d
Matt Rutkowski <mrutkows@us.ibm.com>
2017-03-02 13:14:47 -0600

    # This is a combination of 4 commits. # This is the 1st commit message: Update header to comply with Apache requirements
    Additionally, provide proper header (with script description) using PEP8 compliant string literal.
    
    # The commit message #2 will be skipped:
    
    #	Update box-upload.py
    
    # The commit message #3 will be skipped:
    
    #	Update invoke.py
    
    # The commit message #4 will be skipped:
    
    #	Update actionproxy.py

--- core/actionProxy/actionproxy.py
+++ core/actionProxy/actionproxy.py
@@ -1,18 +1,31 @@
-#
-# Copyright 2015-2016 IBM Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+"""Executable Python script for a proxy service to dockerSkeleton.
+
+Provides a proxy service (using Flask, a Python web microframework)
+that implements the required /init and /run routes to interact with
+the OpenWhisk invoker service.
+
+The implementation of these routes is encapsulated in a class named
+ActionRunner which provides a basic framework for receiving code
+from an invoker, preparing it for execution, and then running the
+code when required.
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+"""
 
 import sys
 import os
--- tools/travis/box-upload.py
+++ tools/travis/box-upload.py
@@ -1,28 +1,52 @@
 #!/usr/bin/env python
+"""Executable Python script for compressing folders to Box.
+
+Compresses the contents of a folder and upload the result to Box.
+
+  Run this script as:
+  $ upload-logs.py LOG_DIR DEST_NAME
+
+  e.g.: $ upload-logs.py /tmp/wsklogs logs-5512.tar.gz
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+"""
+
 import os
 import subprocess
 import sys
 import tempfile
 import urllib
 
-# Compresses the contents of a folder and upload the result to Box.
-# Run this script as:
-#
-# $ upload-logs.py LOG_DIR DEST_NAME
-#
-# e.g.:
-#
-# $ upload-logs.py /tmp/wsklogs logs-5512.tar.gz
 
 def upload_file(local_file, remote_file):
+    """Upload file."""
     if remote_file[0] == '/':
         remote_file = remote_file[1:]
 
-    subprocess.call([ "curl", "-X", "POST", "--data-binary", "@%s" % local_file, "http://wsklogfwd.mybluemix.net/upload?%s" % urllib.urlencode({ "name" : remote_file }) ])
+    subprocess.call(["curl", "-X", "POST", "--data-binary", "@%s" % local_file,
+                     "http://wsklogfwd.mybluemix.net/upload?%s" %
+                     urllib.urlencode({"name": remote_file})])
+
 
 def tar_gz_dir(dir_path):
-    _, dst = tempfile.mkstemp(suffix = ".tar.gz")
-    subprocess.call([ "tar", "-cvzf", dst, dir_path ])
+    """Create TAR (ZIP) of path and its contents."""
+    _, dst = tempfile.mkstemp(suffix=".tar.gz")
+    subprocess.call(["tar", "-cvzf", dst, dir_path])
     return dst
 
 if __name__ == "__main__":
