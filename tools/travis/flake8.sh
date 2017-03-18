550b87440274cc3135c9db0f1692de41703475d5
cclauss <cclauss@bluewin.ch>
2017-03-18 16:53:55 +0100

    Update flake8.sh

--- tools/travis/flake8.sh
+++ tools/travis/flake8.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 PYTHON_FILES=~/openwhisk \
-             ~/openwhisk/tools/admin/wskadmin \
-             ~/openwhisk/tools/build/citool \
-             ~/openwhisk/tools/build/redo \
-             ~/openwhisk/tools/health/isAlive \
-             ~/openwhisk/tools/health/killComponent \
-             ~/openwhisk/tools/health/kvstore
+             ~/openwhisk/openwhisk/tools/admin/wskadmin \
+             ~/openwhisk/openwhisk/tools/build/citool \
+             ~/openwhisk/openwhisk/tools/build/redo \
+             ~/openwhisk/openwhisk/tools/health/isAlive \
+             ~/openwhisk/openwhisk/tools/health/killComponent \
+             ~/openwhisk/openwhisk/tools/health/kvstore
 
 # First round uses --exit-zero to treat every message as a warning
 flake8 $PYTHON_FILES --count --max-line-length=127 --statistics --exit-zero
