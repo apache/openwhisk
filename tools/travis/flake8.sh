fec7349d4497cdd730eb24e3b4b8654187926f0a
cclauss <cclauss@bluewin.ch>
2017-03-18 17:30:05 +0100

    Update flake8.sh

--- tools/travis/flake8.sh
+++ tools/travis/flake8.sh
@@ -1,24 +1,15 @@
 #!/bin/bash
 
-echo "pwd"
-pwd
-echo "ls"
-ls
-echo "~"
-echo ~
-echo "ls"
-ls ~
-echo "done"
+# These files do not have a .py extension so flake8 will not scan them
+PYTHON_FILES=. \
+             ./tools/admin/wskadmin \
+             ./tools/build/citool \
+             ./tools/build/redo \
+             ./tools/health/isAlive \
+             ./tools/health/killComponent \
+             ./tools/health/kvstore
 
-PYTHON_FILES=~ \
-             ~build/openwhisk/openwhisk/tools/admin/wskadmin \
-             ~/tools/build/citool \
-             ~/tools/build/redo \
-             ~/tools/health/isAlive \
-             ~/tools/health/killComponent \
-             ~/tools/health/kvstore
-
-# First round uses --exit-zero to treat every message as a warning
+# First round uses --exit-zero to treat _every_ message as a warning
 flake8 $PYTHON_FILES --count --max-line-length=127 --statistics --exit-zero
 # Second round stops the build if there are any syntax errors
 flake8 PYTHON_FILES --count --max-line-length=127 --select=E999 --statistics
