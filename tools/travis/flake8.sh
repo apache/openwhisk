7b494f060e571db4a61572681cde76b1ecc18abc
cclauss <cclauss@bluewin.ch>
2017-03-19 20:56:30 +0100

    Update flake8.sh

--- tools/travis/flake8.sh
+++ tools/travis/flake8.sh
@@ -1,29 +1,26 @@
 #!/bin/bash
 
 pip3 install --user --upgrade flake8
-python3 -m flake8 --version
+flake8 --version
 
 # These files do not have a .py extension so flake8 will not scan them
-PYTHON_FILES=. \
-             "tools/admin/wskadmin" \
-             "tools/build/citool" \
-             "tools/build/redo" \
-             "tools/health/isAlive" \
-             "tools/health/killComponent" \
-             "tools/health/kvstore"
+declare -a PYTHON_FILES=("."
+                         "./tools/admin/wskadmin"
+                         "./tools/build/citool"
+                         "./tools/build/redo"
+                         "./tools/health/isAlive"
+                         "./tools/health/killComponent"
+                         "./tools/health/kvstore")
 
-echo "==="
-echo $PYTHON_FILES
-echo "==="
-ls -Fla $PYTHON_FILES
-echo "==="
-# First round uses --exit-zero to treat _every_ message as a warning
-python3 -m flake8 $PYTHON_FILES --count --max-line-length=127 --statistics --exit-zero
-# Second round stops the build if there are any syntax errors
-python3 -m flake8 $PYTHON_FILES --count --max-line-length=127 --select=E999 --statistics
-RETURN_CODE=$?
-if [ $RETURN_CODE != 0 ]; then
-    echo "Flake8 found Python 3 syntax errors above.  See: https://docs.python.org/3/howto/pyporting.html"
-    exit $RETURN_CODE
-fi
-9  # stop the build!
+for i in "${PYTHON_FILES[@]}"
+do
+    # First round uses --exit-zero to treat _every_ message as a warning
+    flake8 "$i" --count --max-line-length=127 --statistics --exit-zero
+    # Second round stops the build if there are any syntax errors
+    flake8 "$i" --count --max-line-length=127 --select=E999 --statistics
+    RETURN_CODE=$?
+    if [ $RETURN_CODE != 0 ]; then
+        echo "Flake8 found Python 3 syntax errors above.  See: https://docs.python.org/3/howto/pyporting.html"
+        exit $RETURN_CODE
+    fi
+done
