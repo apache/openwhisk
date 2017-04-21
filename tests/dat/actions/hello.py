f8ee38b6cfc625dacdb6e8a52bb91685646a35b0
cclauss <cclauss@bluewin.ch>
2017-04-21 08:45:33 +0200

    Avoid clobbering Python's built-in dict datatype
    It considered bad practice to name a variable in such a way that it clobbers access to Python's built-in [dict](https://docs.python.org/3/library/stdtypes.html#mapping-types-dict) datatype.

--- tests/dat/actions/hello.py
+++ tests/dat/actions/hello.py
@@ -19,12 +19,9 @@
 """
 
 
-def main(dict):
+def main(args_dict):
     """Main."""
-    if 'name' in dict:
-        name = dict['name']
-    else:
-        name = "stranger"
-    greeting = "Hello " + name + "!"
+    name = args_dict.get('name', 'stranger')
+    greeting = 'Hello ' + name + '!'
     print(greeting)
-    return {"greeting": greeting}
+    return {'greeting': greeting}
