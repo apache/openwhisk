33af4ce377be57962992b85080a80466c66276ec
Vincent Hou <shou@us.ibm.com>
2017-05-19 15:08:44 -0400

    Add the support to install the binaries from the new repo
    This PR adds the functionalities to either install openwhisk
    binaries from the releases page of openwhisk cli or copy the
    binaries locally.
    
    The key installation_mode of openwhisk_cli dict can be set to
    remote or local. The default one is local.
    
    Closes-Bug: #2101

--- ansible/roles/cli/files/createContentJson.sh
+++ ansible/roles/cli/files/createContentJson.sh
@@ -3,69 +3,32 @@
 set +x
 set -e
 
-get_bin_name () {
-  local os=$1
-  local bin="wsk"
-
-  if [ $os = "windows" ]; then
-    bin="${bin}.exe";
-  fi
-
-  echo $bin;
-};
-
-build_cli () {
-  local os=$1
-  local arch=$2
-  local bin=$3
-
-  echo "Building for OS '$os' and architecture '$arch'"
-
-  if [ $os = "mac" ]; then
-    export GOOS=darwin;
-  else
-    export GOOS=$os;
-  fi
-
-  export GOARCH=$arch
-
-  cd /src/github.com/go-whisk-cli
-  go build -ldflags "-X main.CLI_BUILD_TIME=`date -u '+%Y-%m-%dT%H:%M:%S%:z'`" -v -o build/$os/$arch/$bin main.go;
-};
-
 get_compressed_name() {
   local os=$1
   local arch=$2
-  local product_name="OpenWhisk_CLI"
+  local product_name=$3
 
   if [ $arch = amd64 ]; then
-      comp_name="$product_name-$os";
+    comp_name="$product_name-$os";
   elif [ $arch = 386 ]; then
-      comp_name="$product_name-$os-32bit";
+    comp_name="$product_name-$os-32bit";
   else
-      comp_name="$product_name-$os-$arch";
+    comp_name="$product_name-$os-$arch";
   fi
 
   echo $comp_name;
 };
 
-compress_binary() {
+get_binary_path() {
     local comp_name=$1
-    local bin=$2
-    local os=$3
-    local arch=$4
-
-    cd build/$os/$arch
+    local os=$2
+    local arch=$3
 
     if [ $os = "linux" ]; then
-      comp_name="$comp_name.tgz"
-      tar -cvzf $comp_name $bin >/dev/null 2>&1;
+        comp_name="$comp_name.tgz"
     else
-      comp_name="$comp_name.zip"
-      zip $comp_name $bin >/dev/null 2>&1;
+    		comp_name="$comp_name.zip"
     fi
-
-    cd ../../..
     echo $os/$arch/$comp_name;
 };
 
@@ -76,10 +39,8 @@ create_cli_packages() {
     dirIndex="$dirIndex\"$platform\":{"
 
     for arch in $archs; do
-      bin=$(get_bin_name $platform)
-      build_cli $platform $arch $bin
-      comp_name=$(get_compressed_name $platform $arch)
-      comp_path=$(compress_binary $comp_name $bin $platform $arch)
+      comp_name=$(get_compressed_name $platform $arch $zip_name)
+      comp_path=$(get_binary_path $comp_name $platform $arch)
 
       if [ $arch = $default_arch ]; then
           dirIndex="$dirIndex\"default\":{\"path\":\"$comp_path\"},";
@@ -95,11 +56,14 @@ create_cli_packages() {
   dirIndex="$(echo $dirIndex | rev | cut -c2- | rev)"
   dirIndex="$dirIndex}}"
 
-  echo $dirIndex > ./build/content.json
+  echo $dirIndex > $PATH_CONTENT_JSON/content.json
 };
 
-platforms="$CLI_OS"
-archs="$CLI_ARCH";
+archs="386 amd64";
 default_arch="amd64"
 
+PATH_CONTENT_JSON=$1
+BINARY_TAG=$2
+platforms=$3
+zip_name=$4
 create_cli_packages
