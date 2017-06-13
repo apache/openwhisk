90b5a943c8a0a8d99f8d050f1fd6bfb05831e274
Carlos Santana <csantana23@gmail.com>
2017-06-13 15:51:28 -0400

    update ubuntu scripts to fail on first error (#2362)

--- tools/ubuntu-setup/ansible.sh
+++ tools/ubuntu-setup/ansible.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+set -e
+set -x
+
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
-sudo apt-get install python-dev -y
-sudo apt-get install libffi-dev -y
+sudo apt-get install -y python-dev libffi-dev libssl-dev
 sudo pip install markupsafe
 sudo pip install ansible==2.3.0.0
 sudo pip install docker==2.2.1
