d956df1f2173e666fac27dfc10150133dcc4c05c
Rob Allen <rob@akrabat.com>
2017-06-17 19:25:22 +0100

    Implement PHP 7.1 kind
    Build the Docker container from php:7.1-alpine and implement the HTTP
    server using PHP's built in server.
    
    Note that when using a zip file, the router requires that the `main`
    function is stored in `index.php`.

---
+++ core/php7.1Action/router.php
@@ -0,0 +1,272 @@
+<?php
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// set up an output buffer to redirect any script output to stdout, rather than the default
+// php://output, so that it goes to the logs, not the HTTP client
+ob_start(function ($data) {
+    file_put_contents("php://stdout", $data);
+    return '';
+}, 1, PHP_OUTPUT_HANDLER_CLEANABLE | PHP_OUTPUT_HANDLER_FLUSHABLE | PHP_OUTPUT_HANDLER_REMOVABLE);
+
+
+const ACTION_SRC_FILENAME = 'index.php';
+const SRC_DIR  = __DIR__ . '/src';
+const ACTION_CONFIG_FILE = __DIR__. '/config.json';
+const ACTION_SRC_FILE = SRC_DIR . '/' . ACTION_SRC_FILENAME;
+const ACTION_RUNNER_FILE = __DIR__. '/runner.php';
+const TMP_ZIP_FILE = '/action.zip';
+
+main();
+exit;
+
+function main()
+{
+    $output = '';
+    try {
+        switch ($_SERVER['REQUEST_URI']) {
+            case '/init':
+                // this end point is called once per container creation. It gives us the code we need
+                // to run and the name of the function within that code that's the entry point. As PHP
+                // has a setup/teardown model, we store the function name to a config file for retrieval
+                // in the /run end point.
+                $output = init();
+                break;
+
+            case '/run':
+                // this end point is called once per action invocation. We load the function name from
+                // the config file and then invoke it. Note that as PHP writes to php://output, we
+                // capture in an output buffer and write the buffer to stdout for the OpenWhisk logs.
+                $output = run();
+                writeSentinels();
+                break;
+
+            default:
+                throw new RuntimeException('Unexpected call to ' . $_SERVER["REQUEST_URIp"]);
+        }
+    } catch (Throwable $e) {
+        $code = $e->getCode() < 400 ? 500 : $e->getCode();
+
+        file_put_contents("php://stdout", 'Error: ' . $e->getMessage() . "\n");
+        writeSentinels();
+
+        http_response_code($code);
+        $output = json_encode(['error' => $e->getMessage()]);
+    }
+
+    // send response
+    header('Content-Type: application/json');
+    header("Content-Length: " . mb_strlen($output));
+    ob_end_clean();
+    echo $output;
+}
+
+function init()
+{
+    // data is POSTed to us a JSON string
+    $post = file_get_contents('php://input');
+    $data = json_decode($post, true)['value'] ?? [];
+
+    $name = $data['name'] ?? '';         // action name
+    $main = $data['main'] ?? 'main';     // function to call (default: main)
+    $code = trim($data['code'] ?? '');   // source code to run
+    $binary = $data['binary'] ?? false;  // code is binary?
+
+    if (!$code) {
+        throw new RuntimeException("No code to execute");
+    }
+
+    if ($binary) {
+        // binary code is a zip file that's been base64 encoded, so unzip it
+        unzip($code, SRC_DIR);
+
+        // check that we have the expected action source file
+        if (! file_exists(ACTION_SRC_FILE)) {
+            throw new RuntimeException('Zipped actions must contain ' . ACTION_SRC_FILENAME . ' at the root.', 500);
+        }
+    } else {
+        // non-binary code is a text string, so save to disk
+        file_put_contents(ACTION_SRC_FILE, $code);
+    }
+
+    // is action file valid PHP? run `php -l` to find out
+    list($returnCode, $stdout, $stderr) = runPHP(['-l', '-f', ACTION_SRC_FILE]);
+    if ($returnCode != 0) {
+        file_put_contents("php://stderr", $stderr . PHP_EOL);
+        file_put_contents("php://stdout", $stdout);
+
+        $message = 'PHP syntax error in ' . ($binary ? ACTION_SRC_FILENAME : 'action.');
+        throw new RuntimeException($message, 500);
+    }
+
+    // does the action have the expected function name?
+    $testCode = 'require "' . ACTION_SRC_FILENAME . '"; exit((int)(! function_exists("' . $main .'")));';
+    list($returnCode, $stdout, $stderr) = runPHP(['-r', $testCode]);
+    if ($returnCode != 0) {
+        throw new RuntimeException("The function $main is missing.");
+    }
+
+    // write config file for use by /run
+    $config = [
+        'file' => ACTION_SRC_FILENAME,
+        'function' => $main,
+        'name' => $name,
+    ];
+    file_put_contents(ACTION_CONFIG_FILE, json_encode($config));
+
+    return json_encode(["OK" => true]);
+}
+
+function unzip(string $b64Data, $to)
+{
+    file_put_contents(TMP_ZIP_FILE, base64_decode($b64Data));
+
+    $zip = new ZipArchive();
+    $res = $zip->open(TMP_ZIP_FILE);
+    if ($res !== true) {
+        $reasons = [
+            ZipArchive::ER_EXISTS => "File already exists.",
+            ZipArchive::ER_INCONS => "Zip archive inconsistent.",
+            ZipArchive::ER_INVAL => "Invalid argument.",
+            ZipArchive::ER_MEMORY => "Malloc failure.",
+            ZipArchive::ER_NOENT => "No such file.",
+            ZipArchive::ER_NOZIP => "Not a zip archive.",
+            ZipArchive::ER_OPEN => "Can't open file.",
+            ZipArchive::ER_READ => "Read error.",
+            ZipArchive::ER_SEEK => "Seek error.",
+        ];
+        $reason = $reasons[$res] ?? "Unknown error: $res.";
+        throw new RuntimeException("Failed to open zip file: $reason", 500);
+    }
+
+    $res = $zip->extractTo($to . '/');
+    $zip->close();
+}
+
+function run()
+{
+    if (! file_exists(ACTION_SRC_FILE)) {
+        error_log('NO ACTION FILE: ' . ACTION_SRC_FILE);
+        throw new RuntimeException('!Could not find action file: ' . ACTION_SRC_FILENAME, 500);
+    }
+
+    // load config to pass to runner
+    if (! file_exists(ACTION_CONFIG_FILE)) {
+        error_log('NO CONFIG FILE: ' . ACTION_CONFIG_FILE);
+        throw new RuntimeException('Could not find config file', 500);
+    }
+    $config = file_get_contents(ACTION_CONFIG_FILE);
+    
+    // Extract the posted data
+    $post = json_decode(file_get_contents('php://input'), true);
+    if (!is_array($post)) {
+        $post = [];
+    }
+
+    // assign environment variables from the posted data
+    $env = array_filter($_ENV, function ($k) {
+        // only pass OpenWhisk environment variables to the action
+        return stripos($k, '__OW_') === 0;
+    }, ARRAY_FILTER_USE_KEY);
+    foreach (['api_key', 'namespace', 'action_name', 'activation_id', 'deadline'] as $param) {
+        if (array_key_exists($param, $post)) {
+            $env['__OW_' . strtoupper($param)] = $post[$param];
+        }
+    }
+
+    // extract the function arguments from the posted data's "value" field
+    $args = [];
+    if (array_key_exists('value', $post) && is_array($post['value'])) {
+        $args = $post['value'];
+    }
+
+    // run the action
+    list($returnCode, $stdout, $stderr) = runPHP(
+        ['-d', 'error_reporting=E_ALL', '-f', ACTION_RUNNER_FILE, '--', $config],
+        json_encode((object)$args),
+        $env
+    );
+
+    // separate the esponse to send back to the client: it's the last line of stdout
+    $pos = strrpos($stdout, PHP_EOL);
+    if ($pos == false) {
+        // just one line of output
+        $output = $stdout;
+        $stdout = '';
+    } else {
+        $pos++;
+        $output = substr($stdout, $pos);
+        $stdout = substr($stdout, 0, $pos);
+    }
+    // write out the action's stderr and stdout
+    file_put_contents("php://stderr", $stderr . PHP_EOL);
+    file_put_contents("php://stdout", $stdout);
+
+    if ($returnCode != 0 || !is_array(json_decode($output, true))) {
+        // an error occurred while running the action as either the return code is non-zero
+        // or we didn't get JSON back
+        $message = $output ?? 'An error occurred running the action';
+        throw new RuntimeException($message, 502);
+    }
+
+    return $output;
+}
+
+function writeSentinels()
+{
+    // write out sentinels as we've finished all log output
+    file_put_contents("php://stderr", "\nXXX_THE_END_OF_A_WHISK_ACTIVATION_XXX\n");
+    file_put_contents("php://stdout", "XXX_THE_END_OF_A_WHISK_ACTIVATION_XXX\n");
+}
+
+function runPHP(array $args, string $stdin = '', array $env = [])
+{
+    $cmd = '/usr/local/bin/php ' . implode(' ', array_map('escapeshellarg', $args));
+
+    $process = proc_open(
+        $cmd,
+        [
+            0 => ['pipe', 'r'],
+            1 => ['pipe', 'w'],
+            2 => ['pipe', 'w'],
+        ],
+        $pipes,
+        SRC_DIR,
+        $env
+    );
+
+    // write to the process' stdin
+    $bytes = fwrite($pipes[0], $stdin);
+    fclose($pipes[0]);
+
+    // read the process' stdout
+    $stdout = stream_get_contents($pipes[1]);
+    fclose($pipes[1]);
+
+    // read the process' stderr
+    $stderr = stream_get_contents($pipes[2]);
+    fclose($pipes[2]);
+
+    // close process & get return code
+    $returnCode = proc_close($process);
+
+    // tidy up paths in any PHP stack traces
+    $stderr = str_replace(__DIR__ . '/', '', trim($stderr));
+    $stdout = str_replace(__DIR__ . '/', '', trim($stdout));
+
+    return [$returnCode, $stdout, $stderr];
+}
