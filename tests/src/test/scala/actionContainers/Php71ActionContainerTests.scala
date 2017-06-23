374e72311fd1ad7c932fc918af2a997a1d162190
Rob Allen <rob@akrabat.com>
2017-06-23 14:05:27 +0100

    Update licenses

--- tests/src/test/scala/actionContainers/Php71ActionContainerTests.scala
+++ tests/src/test/scala/actionContainers/Php71ActionContainerTests.scala
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package actionContainers
 
 import org.junit.runner.RunWith
