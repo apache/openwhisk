55e693a4d91ea18aed0a01740db0ac2b9f56cddb
Dominic Kim <style9595@gmail.com>
2017-06-28 09:22:18 +0900

    Make OW run with docker for mac (#1790)
    * Add documentation for docker for mac (#1431)
    * Replace akka-http client to play-ws client (temporarily)
    * Introduce docker-machine environment

--- core/invoker/src/main/scala/whisk/core/container/WhiskContainer.scala
+++ core/invoker/src/main/scala/whisk/core/container/WhiskContainer.scala
@@ -21,18 +21,9 @@ import java.time.Clock
 import java.time.Instant
 import java.util.concurrent.atomic.AtomicInteger
 
-import scala.concurrent.Await
-import scala.concurrent.Future
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.duration.FiniteDuration
-
 import akka.actor.ActorSystem
-import akka.http.scaladsl.Http
-import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.marshalling._
-import akka.http.scaladsl.model._
-import akka.http.scaladsl.unmarshalling._
-import akka.stream.ActorMaterializer
 import spray.json._
 import whisk.common.Logging
 import whisk.common.LoggingMarkers
@@ -142,60 +133,6 @@ class WhiskContainer(
      */
     private def sendPayload(endpoint: String, msg: JsObject, timeout: FiniteDuration, retry: Boolean)(implicit system: ActorSystem): RunResult = {
         sendPayloadApache(endpoint, msg, timeout, retry)
-    }
-
-    private def sendPayloadAkka(endpoint: String, msg: JsObject, timeout: FiniteDuration)(implicit system: ActorSystem): RunResult = {
-        import system.dispatcher
-
-        val start = ContainerCounter.now()
-
-        val f = sendPayloadAsync(endpoint, msg, timeout)
-
-        f.onFailure {
-            case t: Throwable =>
-                logging.warn(this, s"Exception while posting to action container ${t.getMessage}")
-        }
-
-        // Should never timeout because the future has a built-in timeout.
-        // Keeping a finite duration for safety.
-        Await.ready(f, timeout + 1.minute)
-
-        val end = ContainerCounter.now()
-
-        val r = f.value.get.toOption.flatten
-        RunResult(Interval(start, end), ???)
-    }
-
-    /**
-     * Asynchronously posts a message to the container.
-     *
-     *  @param msg the message to post
-     *  @return response from the container if any
-     */
-    private def sendPayloadAsync(endpoint: String, msg: JsObject, timeout: FiniteDuration)(implicit system: ActorSystem): Future[Option[(Int, String)]] = {
-        implicit val ec = system.dispatcher
-        implicit val materializer = ActorMaterializer()
-
-        containerHostAndPort map { hp =>
-
-            val flow = Http().outgoingConnection(hp.host, hp.port)
-
-            val uri = Uri(
-                scheme = "http",
-                authority = Uri.Authority(host = Uri.Host(hp.host), port = hp.port),
-                path = Uri.Path(endpoint))
-
-            for (
-                entity <- Marshal(msg).to[MessageEntity];
-                request = HttpRequest(method = HttpMethods.POST, uri = uri, entity = entity);
-                response <- AkkaHttpUtils.singleRequest(request, timeout, retryOnTCPErrors = true, retryInterval = 100.milliseconds);
-                responseBody <- Unmarshal(response.entity).to[String]
-            ) yield {
-                Some((response.status.intValue, responseBody))
-            }
-        } getOrElse {
-            Future.successful(None)
-        }
     }
 
     private def sendPayloadApache(endpoint: String, msg: JsObject, timeout: FiniteDuration, retry: Boolean): RunResult = {
--- tests/src/test/scala/actionContainers/ActionContainer.scala
+++ tests/src/test/scala/actionContainers/ActionContainer.scala
@@ -161,12 +161,10 @@ object ActionContainer {
 
     private def syncPost(host: String, port: Int, endPoint: String, content: JsValue)(
         implicit actorSystem: ActorSystem): (Int, Option[JsObject]) = {
-        import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-        import akka.http.scaladsl.marshalling._
         import akka.http.scaladsl.model._
         import akka.http.scaladsl.unmarshalling._
         import akka.stream.ActorMaterializer
-        import whisk.core.container.AkkaHttpUtils
+        import common.AkkaHttpUtils
 
         implicit val materializer = ActorMaterializer()
 
@@ -176,10 +174,8 @@ object ActionContainer {
             path = Uri.Path(endPoint))
 
         val f = for (
-            entity <- Marshal(content).to[MessageEntity];
-            request = HttpRequest(method = HttpMethods.POST, uri = uri, entity = entity);
-            response <- AkkaHttpUtils.singleRequest(request, 90.seconds, retryOnTCPErrors = true);
-            responseBody <- Unmarshal(response.entity).to[String]
+            response <- AkkaHttpUtils.singleRequest(uri.toString(), content, 90.seconds, retryOnTCPErrors = true);
+            responseBody <- Unmarshal(response.body).to[String]
         ) yield (response.status.intValue, Try(responseBody.parseJson.asJsObject).toOption)
 
         Await.result(f, 90.seconds)
