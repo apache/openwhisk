41a8251ae248221434884d5387557f98da52af97
Matt Rutkowski <mrutkows@us.ibm.com>
2017-06-30 20:39:19 -0500

    Update remaining files to ASF license incl. bash, js files. (#2423)

--- core/routemgmt/common/apigw-utils.js
+++ core/routemgmt/common/apigw-utils.js
@@ -1,21 +1,23 @@
-/**
- * Copyright 2015-2017 IBM Corporation
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
+ */
+
+/**
  * Route management action common API GW utilities
- *
- **/
+ */
 var request = require('request');
 var _ = require('lodash');
 
@@ -131,7 +133,7 @@ function deleteApiFromGateway(gwInfo, spaceGuid, apiId) {
   });
 }
 
-/*
+/**
  * Return an array of APIs
  */
 function getApis(gwInfo, spaceGuid, bpOrApiName) {
@@ -641,7 +643,6 @@ function getHostFromActionUrl(actionUrl) {
  *             result[1] : action name
  *             result[2] : ''
  *             result[3] : ''
-
  */
 function parseActionName(fqname) {
   console.log('parseActionName: parsing action: '+fqname);
