b1c34ed587efebc79539850e7065cf3e760b3c7d
Markus Thömmes <markusthoemmes@me.com>
2017-07-01 15:20:32 +0200

    Adjust kafka's maximum poll period. (#2450)
    Kafka allows setting the maximum period between two polls. As we allow for 5 minutes of action execution, in a fully loaded invoker it might take 5 minutes for the consumer to poll again. To prevent rebalances this value is adjusted accordingly.
    
    Bumping Kafka's version is needed because that value is exposed only in 0.10.1+.

--- core/invoker/src/main/scala/whisk/core/invoker/Invoker.scala
+++ core/invoker/src/main/scala/whisk/core/invoker/Invoker.scala
@@ -52,6 +52,7 @@ import whisk.core.connector.PingMessage
 import scala.util.Try
 import whisk.core.connector.MessageProducer
 import org.apache.kafka.common.errors.RecordTooLargeException
+import whisk.core.entity.TimeLimit
 
 /**
  * A kafka message handler that invokes actions as directed by message on topic "/actions/invoke".
@@ -484,9 +485,8 @@ object Invoker {
         }
 
         val topic = s"invoker${invokerInstance.toInt}"
-        val groupid = "invokers"
         val maxdepth = ContainerPool.getDefaultMaxActive(config)
-        val consumer = new KafkaConsumerConnector(config.kafkaHost, groupid, topic, maxdepth)
+        val consumer = new KafkaConsumerConnector(config.kafkaHost, "invokers", topic, maxdepth, maxPollInterval = TimeLimit.MAX_DURATION + 1.minute)
         val producer = new KafkaProducerConnector(config.kafkaHost, ec)
         val dispatcher = new Dispatcher(consumer, 500 milliseconds, 2 * maxdepth, actorSystem)
 
