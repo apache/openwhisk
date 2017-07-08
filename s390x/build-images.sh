92e270b3a6efb0dc9c03b0c74ee2e4a7f1837319
Jonathan Springer <jonpspri@gmail.com>
2017-07-08 19:11:19 -0400

    Move apigateway to simple directory

--- s390x/build-images.sh
+++ s390x/build-images.sh
@@ -14,21 +14,43 @@ REPOSITORY=${REPOSITORY:-jpspring/s390x-openwhisk}
 
 cd "$( dirname "${BASH_SOURCE[0]}" )" || exit
 
+arch=$(docker info 2>/dev/null | sed -n 's/Architecture: \(.*\)/\1/p')
+echo "Processing for $arch"
+
 process_dockerfile() {
-  label=$(grep '^#LABEL ' "$1" | sed -n 's/^#LABEL \(.*\)$/\1/p')
-  [ -d "./$label" ] && dir="./$label" || dir="."
-  docker build -t "$REPOSITORY:$label" -f "$1" "$dir" || {
+  local tmpfile, dockerfile
+
+  if [ -f "$1/Dockerfile.$arch" ]; then dockerfile="$1/Dockerfile.$arch"
+  elif [ -f "$1/Dockerfile.m4" ]; then
+    tmpfile="$(mktemp -tp "$1")"
+    echo "...Pre-processing $1/Dockerfile.m4 to $tmpfile"
+    m4 -P \
+      -D "$(echo "$arch" | tr '[:lower:]' '[:upper:]')" -D "m4_dockerarch=$arch" \
+		  "$1/Dockerfile.m4" > $tmpfile
+    dockerfile="$tmpfile"
+  elif [ -f "$1/Dockerfile" ]; then dockerfile="$1/Dockerfile"
+  else exit 1
+  fi
+
+  echo "...Processing $dockerfile"
+
+  label=$(grep '^#LABEL ' "$dockerfile" | sed -n 's/^#LABEL \(.*\)$/\1/p')
+  [ -d "./$label" ] && dir="./$label" || dir="$1"
+  docker build -t "$REPOSITORY:$label" -f "$dockerfile" "$dir" || {
     echo Failure building $1; exit 1
   }
-  docker push "jpspring/s390x-openwhisk:$label"
+  docker push "$REPOSITORY:$label"
 
   for i in "$(dirname $1)"/test_*; do
     if [[ -x "$i" ]]; then eval "$i"; fi
   done
+
+  [ -n "$tmpfile" ] && rm "$tmpfile"
 }
 
 if [ "$#" -eq 0 ]; then
-  for i in */Dockerfile?(.m4); do process_dockerfile "$i"; done
+  compgen -G '*/Dockerfile*' | xargs dirname | uniq | \
+  while read i; do process_dockerfile "$i"; done
 else
-  while [ "$#" -ne 0 ]; do process_dockerfile "$1/Dockerfile"; shift; done
+  while [ "$#" -ne 0 ]; do [ -d "$1" ] && process_dockerfile "$1"; shift; done
 fi
