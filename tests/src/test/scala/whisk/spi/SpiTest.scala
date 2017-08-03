adc4f8bd873a9f8b76949ed3ce9760245ec6924c
Markus Thoemmes <markus.thoemmes@de.ibm.com>
2017-08-03 19:54:11 +0200

    Fixing testcase, using matchers in tests

--- tests/src/test/scala/whisk/spi/SpiTest.scala
+++ tests/src/test/scala/whisk/spi/SpiTest.scala
@@ -14,13 +14,9 @@ class SpiTest extends FlatSpec with Matchers with WskActorSystem with StreamLogg
 
     behavior of "SpiProvider"
 
-    it should "load an Spi from SpiLoader via classname" in {
-        val simpleSpi: SimpleSpi = SpiLoader.get[SimpleSpi]()
-        assert(simpleSpi != null)
-    }
     it should "load an Spi from SpiLoader via typesafe config" in {
-        val simpleSpi: SimpleSpi = SpiLoader.get[SimpleSpi]()
-        assert(simpleSpi != null)
+        val simpleSpi = SpiLoader.get[SimpleSpi]()
+        simpleSpi shouldBe a[SimpleSpi]
     }
 
     it should "throw an exception if the impl defined in application.conf is missing" in {
@@ -36,30 +32,32 @@ class SpiTest extends FlatSpec with Matchers with WskActorSystem with StreamLogg
     it should "load an Spi with injected WhiskConfig" in {
         val whiskConfig = new WhiskConfig(Map())
         val deps = Dependencies("some name", whiskConfig)
-        val dependentSpi: DependentSpi = SpiLoader.get[DependentSpi](deps)
-        assert(dependentSpi.config.eq(whiskConfig))
+        val dependentSpi = SpiLoader.get[DependentSpi](deps)
+        dependentSpi.config shouldBe whiskConfig
     }
     //
     it should "load an Spi with injected Spi" in {
         val whiskConfig = new WhiskConfig(Map())
         val deps = Dependencies("some name", whiskConfig)
-        val dependentSpi: DependentSpi = SpiLoader.get[DependentSpi](deps)
+        val dependentSpi = SpiLoader.get[DependentSpi](deps)
 
         val deps2 = Dependencies("dep2", dependentSpi)
-        val testSpi: TestSpi = SpiLoader.get[TestSpi](deps2)
-        assert(testSpi.dep.eq(dependentSpi))
+        val testSpi = SpiLoader.get[TestSpi](deps2)
+
+        testSpi.dep shouldBe dependentSpi
     }
     //
     it should "not allow duplicate-type dependencies" in {
-        a[IllegalArgumentException] should be thrownBy  Dependencies("some string", "some other string")
+        a[IllegalArgumentException] should be thrownBy Dependencies("some string", "some other string")
     }
     //
     it should "load SPI impls as singletons via SingletonSpiFactory" in {
         val instance1 = SpiLoader.get[DependentSpi]()
         val instance2 = SpiLoader.get[DependentSpi]()
         val instance3 = SpiLoader.get[DependentSpi]()
-        assert(instance1.hashCode() == instance2.hashCode())
 
+        instance1 shouldBe instance2
+        instance2 shouldBe instance3
     }
 
     //
@@ -67,8 +65,9 @@ class SpiTest extends FlatSpec with Matchers with WskActorSystem with StreamLogg
         val instance1 = SpiLoader.get[SimpleSpi]()
         val instance2 = SpiLoader.get[SimpleSpi]()
         val instance3 = SpiLoader.get[SimpleSpi]()
-        assert(instance1.hashCode() == instance2.hashCode())
 
+        instance1 shouldBe instance2
+        instance2 shouldBe instance3
     }
 }
 
@@ -83,11 +82,11 @@ trait DependentSpi extends Spi {
 }
 
 trait TestSpiFactory extends Spi {
-    def getTestSpi(name: String, dep: DependentSpi):TestSpi
+    def getTestSpi(name: String, dep: DependentSpi): TestSpi
 }
 
 trait DependentSpiFactory extends Spi {
-    def getDependentSpi(name: String, config: WhiskConfig):DependentSpi
+    def getDependentSpi(name: String, config: WhiskConfig): DependentSpi
 }
 
 abstract class Key(key: String) {
@@ -111,7 +110,7 @@ trait MissingKey extends Spi
 //a singleton enforced by SingletonSpiFactory
 class DepSpiImpl(val name: String, val config: WhiskConfig) extends DependentSpi
 object DepSpiImpl extends SingletonSpiFactory[DependentSpi] {
-    override def buildInstance(deps:Dependencies): DependentSpi = {
+    override def apply(deps: Dependencies): DependentSpi = {
         new DepSpiImpl(deps.get[String], deps.get[WhiskConfig])
     }
 }
@@ -119,9 +118,9 @@ object DepSpiImpl extends SingletonSpiFactory[DependentSpi] {
 class TestSpiImpl(val name: String, val dep: DependentSpi) extends TestSpi
 //an alternative to extending SingletonSpiFactory is using lazy val:
 object TestSpiImpl extends SpiFactory[TestSpi] {
-    var name:String = null
-    var conf:DependentSpi = null
-    lazy val instance =  new TestSpiImpl(name, conf)
+    var name: String = null
+    var conf: DependentSpi = null
+    lazy val instance = new TestSpiImpl(name, conf)
     override def apply(dependencies: Dependencies): TestSpi = {
         name = dependencies.get[String]
         conf = dependencies.get[DependentSpi]
@@ -130,14 +129,12 @@ object TestSpiImpl extends SpiFactory[TestSpi] {
 
 }
 
-
-
 class TestSpiFactoryImpl extends TestSpiFactory {
     def getTestSpi(name: String, dep: DependentSpi) = new TestSpiImpl(name, dep)
 }
 
 object TestSpiFactoryImpl extends SpiFactory[TestSpiFactory] {
-    override def apply(deps:Dependencies): TestSpiFactory = new TestSpiFactoryImpl()
+    override def apply(deps: Dependencies): TestSpiFactory = new TestSpiFactoryImpl()
 }
 
 class DependentSpiFactoryImpl extends DependentSpiFactory {
@@ -145,17 +142,17 @@ class DependentSpiFactoryImpl extends DependentSpiFactory {
 }
 
 object DependentSpiFactoryImpl extends SpiFactory[DependentSpiFactory] {
-    override def apply(deps:Dependencies): DependentSpiFactory = new DependentSpiFactoryImpl()
+    override def apply(deps: Dependencies): DependentSpiFactory = new DependentSpiFactoryImpl()
 }
 
 class SimpleSpiImpl(val name: String) extends SimpleSpi
 
 object SimpleSpiImpl extends SingletonSpiFactory[SimpleSpi] {
-    override def buildInstance(dependencies: Dependencies): SimpleSpi = new SimpleSpiImpl("some val ")
+    override def apply(dependencies: Dependencies): SimpleSpi = new SimpleSpiImpl("some val ")
 }
 
 class MissingSpiImpl(val name: String) extends MissingSpi
 
 object MissingSpiImpl extends SpiFactory[MissingSpi] {
-    override def apply(deps:Dependencies): MissingSpi = new MissingSpiImpl("some val ")
+    override def apply(deps: Dependencies): MissingSpi = new MissingSpiImpl("some val ")
 }
