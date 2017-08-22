ningyougang/incubator-openwhisk

Support client certificate verify on server side
Currently, openwhisk supports passing client certificate from wsk to
nginx, but for nginx verification, it just verifies the client certificate's
format whether right, in order to complete the whole verification flow,
it is necessary to verify the CN value which is saved in cert's subject
info whether matches the user saved in couchdb.

tools/cli/go-whisk-cli/commands/api.go
@@ -1547,9 +1547,11 @@ func getUserContextId() (string, error) {
         if len(props["AUTH"]) > 0 {
             guid = strings.Split(props["AUTH"], ":")[0]
         } else {
-            whisk.Debug(whisk.DbgError, "AUTH property not set in properties file: %s\n", Properties.PropsFile)
-            errStr := wski18n.T("Authorization key is not configured (--auth is required)")
-            err = whisk.MakeWskError(errors.New(errStr), whisk.EXITCODE_ERR_GENERAL, whisk.DISPLAY_MSG, whisk.NO_DISPLAY_USAGE)
+            if len(props["CERT"]) == 0 || len(props["KEY"]) == 0 {
+                whisk.Debug(whisk.DbgError, "AUTH property or CERT/KEY property also not set in properties file: %s\n", Properties.PropsFile)
+                errStr := wski18n.T("Authorization key or client certificate is not configured (--auth or (--cert and --key) is required)")
+                err = whisk.MakeWskError(errors.New(errStr), whisk.EXITCODE_ERR_GENERAL, whisk.DISPLAY_MSG, whisk.NO_DISPLAY_USAGE)
+            }
         }
     } else {
         whisk.Debug(whisk.DbgError, "readProps(%s) failed: %s\n", Properties.PropsFile, err)

tools/cli/go-whisk/whisk/client.go
@@ -214,10 +214,12 @@ func (c *Client) addAuthHeader(req *http.Request, authRequired bool) error {
         Debug(DbgInfo, "Adding basic auth header; using authkey\n")
     } else {
         if authRequired {
-            Debug(DbgError, "The required authorization key is not configured - neither set as a property nor set via the --auth CLI argument\n")
-            errStr := wski18n.T("Authorization key is not configured (--auth is required)")
-            werr := MakeWskError(errors.New(errStr), EXITCODE_ERR_USAGE, DISPLAY_MSG, DISPLAY_USAGE)
-            return werr
+            if c.Config.Cert == "" || c.Config.Key == "" {
+                Debug(DbgError, "The required authorization key or client certificate is not configured - neither set as a property nor set via the --auth or (--cert and --key)CLI argument\n")
+                errStr := wski18n.T("Authorization key or client certificate is not configured (--auth or (--cert and --key) is required)")
+                werr := MakeWskError(errors.New(errStr), EXITCODE_ERR_USAGE, DISPLAY_MSG, DISPLAY_USAGE)
+                return werr
+            }
         }
     }
     return nil

apache/openwhisk

tools/cli/go-whisk-cli/commands/api.go
@@ -925,9 +925,11 @@ func getUserContextId() (string, error) {
         if len(props["AUTH"]) > 0 {
             guid = strings.Split(props["AUTH"], ":")[0]
         } else {
-            whisk.Debug(whisk.DbgError, "AUTH property not set in properties file: %s\n", Properties.PropsFile)
-            errStr := wski18n.T("Authorization key is not configured (--auth is required)")
-            err = whisk.MakeWskError(errors.New(errStr), whisk.EXIT_CODE_ERR_GENERAL, whisk.DISPLAY_MSG, whisk.NO_DISPLAY_USAGE)
+            if len(props["CERT"]) == 0 || len(props["KEY"]) == 0 {
+                whisk.Debug(whisk.DbgError, "AUTH property or CERT/KEY property also not set in properties file: %s\n", Properties.PropsFile)
+                errStr := wski18n.T("Authorization key or client certificate is not configured (--auth or (--cert and --key) is required)")
+                err = whisk.MakeWskError(errors.New(errStr), whisk.EXIT_CODE_ERR_GENERAL, whisk.DISPLAY_MSG, whisk.NO_DISPLAY_USAGE)
+            }
         }
     } else {
         whisk.Debug(whisk.DbgError, "readProps(%s) failed: %s\n", Properties.PropsFile, err)

tools/cli/go-whisk/whisk/client.go
@@ -67,6 +67,7 @@ type Client struct {
 
 type Config struct {
     Namespace 	string // NOTE :: Default is "_"
+    NamespaceHeader string //Pass namespace by header when using client certificate auth.
     Cert        string
     Key         string
     AuthToken 	string
@@ -90,6 +91,7 @@ func NewClient(httpClient *http.Client, config *Config) (*Client, error) {
                     Certificates: []tls.Certificate{cert},
                     InsecureSkipVerify: true,
                 }
+                config.NamespaceHeader = config.Namespace
             }
         }else{
             tlsConfig = &tls.Config{
@@ -214,10 +216,15 @@ func (c *Client) addAuthHeader(req *http.Request, authRequired bool) error {
         Debug(DbgInfo, "Adding basic auth header; using authkey\n")
     } else {
         if authRequired {
-            Debug(DbgError, "The required authorization key is not configured - neither set as a property nor set via the --auth CLI argument\n")
-            errStr := wski18n.T("Authorization key is not configured (--auth is required)")
-            werr := MakeWskError(errors.New(errStr), EXIT_CODE_ERR_USAGE, DISPLAY_MSG, DISPLAY_USAGE)
-            return werr
+            if c.Config.Cert == "" || c.Config.Key == "" {
+                Debug(DbgError, "The required authorization key or client certificate is not configured - neither set as a property nor set via the --auth or (--cert and --key)CLI argument\n")
+                errStr := wski18n.T("Authorization key or client certificate is not configured (--auth or (--cert and --key) is required)")
+                werr := MakeWskError(errors.New(errStr), EXIT_CODE_ERR_USAGE, DISPLAY_MSG, DISPLAY_USAGE)
+                return werr
+            }else{
+                //Pass namespace by header when using client certificate auth.
+                req.Header.Add("namespace", c.Config.NamespaceHeader)
+            }
         }
     }
     return nil