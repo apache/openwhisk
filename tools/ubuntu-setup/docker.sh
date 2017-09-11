4643dbb24b4c11ab7fa22c45fe18679ab13daf31
Strajan Sebastian Ioan <strajan.sebastian@yahoo.com>
2017-09-11 19:16:59 +0300

    add support for multiple versions of ubuntu
    ubuntu 16.04 and any other distros than trusty may encounter some problems because of hardcoded values

--- tools/ubuntu-setup/docker.sh
+++ tools/ubuntu-setup/docker.sh
@@ -4,7 +4,7 @@ set -x
 
 sudo apt-get -y install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sudo sh -c "echo deb https://apt.dockerproject.org/repo ubuntu-trusty main  > /etc/apt/sources.list.d/docker.list"
+sudo sh -c "echo deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -c -s) main  > /etc/apt/sources.list.d/docker.list"
 sudo apt-get -y update -qq
 
 sudo apt-get purge lxc-docker
@@ -14,7 +14,7 @@ sudo apt-cache policy docker-engine
 sudo apt-get -y install linux-image-extra-$(uname -r)
 
 # DOCKER
-sudo apt-get install -y --force-yes docker-engine=1.12.0-0~trusty
+sudo apt-get install -y --force-yes docker-engine
 
 # enable (security - use 127.0.0.1)
 sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --storage-driver=aufs"'\'' >> /etc/default/docker'
