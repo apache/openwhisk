ac44a6fe2b7c8e129b521fdecbbe6ead8d39a666
Vadim Raskin <vadimr@de.ibm.com>
2017-09-14 08:36:26 +0200

    Configure jmxremote

--- core/controller/init.sh
+++ core/controller/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $( ls /logs/jmxremote.* 2> /dev/null ) ]]
+then
+  mv /logs/jmxremote.* /root ; chmod 600 /root/jmxremote.*
+fi
+
 export CONTROLLER_OPTS
 CONTROLLER_OPTS="$CONTROLLER_OPTS -Dakka.remote.netty.tcp.bind-hostname=$(hostname -I) $(./transformEnvironment.sh)"
 
--- core/invoker/init.sh
+++ core/invoker/init.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $( ls /logs/jmxremote.* 2> /dev/null ) ]]
+then
+  mv /logs/jmxremote.* /root ; chmod 600 /root/jmxremote.*
+fi
+
 export INVOKER_OPTS
 INVOKER_OPTS="$INVOKER_OPTS $(./transformEnvironment.sh)"
 
