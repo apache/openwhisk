c18e0ca341ea4c4f610b2ecee197c50d76ac9091
Brendan McAdams <bmcadams@redhat.com>
2017-10-16 13:36:42 -0700

    Spacing fix for style guidelines

--- core/invoker/src/main/scala/whisk/core/containerpool/ContainerPool.scala
+++ core/invoker/src/main/scala/whisk/core/containerpool/ContainerPool.scala
@@ -116,7 +116,7 @@ class ContainerPool(childFactory: ActorRefFactory => ActorRef,
     // Container is free to take more work
     case NeedWork(data: WarmedData) =>
       freePool = freePool + (sender() -> data)
-      busyPool.get(sender()).foreach { _=>
+      busyPool.get(sender()).foreach { _ =>
         busyPool = busyPool - sender()
         feed ! MessageFeed.Processed
       }
@@ -128,7 +128,7 @@ class ContainerPool(childFactory: ActorRefFactory => ActorRef,
     // Container got removed
     case ContainerRemoved =>
       freePool = freePool - sender()
-      busyPool.get(sender()).foreach { _=>
+      busyPool.get(sender()).foreach { _ =>
         busyPool = busyPool - sender()
         feed ! MessageFeed.Processed
       }
