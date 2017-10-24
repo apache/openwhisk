ec5cb8efd8a641c23cfdc9ffaf28ce68c62eaea2
Brendan McAdams <bmcadams@redhat.com>
2017-10-24 12:20:34 -0700

    Update Vagrant image to latest Ubuntu LTS release; adjust Docker tools accordingly - The version of Ubuntu being used was 14.04 (Trusty) which is EOL and 3+ years old    - Updated to 16.04, Xenial, which is most current LTS    - Moved from Ubuntu images to Chef Bento – Ubuntu's images after 14.04 started      breaking Vagrant convention by not creating a "vagrant" user. Chef Bento images      keep with Vagrant convention and obviate the need for extra "create user" scripts - Updated Ubuntu Tools Docker image accordingly

--- tools/ubuntu-setup/docker.sh
+++ tools/ubuntu-setup/docker.sh
@@ -3,28 +3,32 @@ set -e
 set -x
 
 sudo apt-get -y install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sudo sh -c "echo deb https://apt.dockerproject.org/repo ubuntu-trusty main  > /etc/apt/sources.list.d/docker.list"
+
+# Docker GPG Key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
 sudo apt-get -y update -qq
 
-sudo apt-get purge lxc-docker
-sudo apt-cache policy docker-engine
-
 # AUFS
-sudo apt-get -y install linux-image-extra-$(uname -r)
+sudo apt-get -y install linux-image-extra-$(uname -r) linux-image-extra-virtual
 
 # DOCKER
-sudo apt-get install -y --force-yes docker-engine=1.12.0-0~trusty
+sudo apt-get install -y --force-yes docker-ce
 
 # enable (security - use 127.0.0.1)
-sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --storage-driver=aufs"'\'' >> /etc/default/docker'
+#sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --storage-driver=aufs"'\'' >> /etc/default/docker'
 sudo gpasswd -a `whoami` docker
 
 # Set DOCKER_HOST as an environment variable
-sudo -E bash -c 'echo '\''export DOCKER_HOST="tcp://0.0.0.0:4243"'\'' >> /etc/bash.bashrc'
-source /etc/bash.bashrc
+#sudo -E bash -c 'echo '\''export DOCKER_HOST="tcp://0.0.0.0:4243"'\'' >> /etc/bash.bashrc'
+#source /etc/bash.bashrc
 
-sudo service docker restart
+#sudo service docker restart
 
 # do not run this command without a vagrant reload during provisioning
 # it gives an error that docker is not up (which the reload fixes).
