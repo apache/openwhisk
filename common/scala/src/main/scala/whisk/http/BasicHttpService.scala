ccf8b8c4b28c39da10ccab305596d131b62d6a2a
Vadim Raskin <vadimr@de.ibm.com>
2017-11-20 17:27:49 +0100

    Add header for extra logging
    Signed-off-by: Martin Henke <martin.henke@de.ibm.com>

--- common/scala/src/main/scala/whisk/http/BasicHttpService.scala
+++ common/scala/src/main/scala/whisk/http/BasicHttpService.scala
@@ -27,8 +27,7 @@ import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.HttpRequest
 import akka.http.scaladsl.server._
 import akka.http.scaladsl.server.RouteResult.Rejected
-import akka.http.scaladsl.server.directives.DebuggingDirectives
-import akka.http.scaladsl.server.directives.LogEntry
+import akka.http.scaladsl.server.directives.{DebuggingDirectives, HeaderDirectives, LogEntry}
 import akka.stream.ActorMaterializer
 import spray.json._
 import whisk.common.LogMarker
@@ -43,6 +42,8 @@ import whisk.common.MetricEmitter
  * facilities common to all OpenWhisk REST services.
  */
 trait BasicHttpService extends Directives with TransactionCounter {
+
+  val OW_EXTRA_LOGGING_HEADER = "X-OW-EXTRA-LOGGING"
 
   /**
    * Gets the routes implemented by the HTTP service.
@@ -90,7 +91,13 @@ trait BasicHttpService extends Directives with TransactionCounter {
   }
 
   /** Assigns transaction id to every request. */
-  protected val assignId = extract(_ => transid())
+  protected def assignId = HeaderDirectives.optionalHeaderValueByName(OW_EXTRA_LOGGING_HEADER) flatMap { headerValue =>
+    val extraLogging = headerValue match {
+      case Some(value) => value.toLowerCase == "on"
+      case None        => false
+    }
+    extract(_ => transid(extraLogging))
+  }
 
   /** Generates log entry for every request. */
   protected def logRequestInfo(req: HttpRequest)(implicit tid: TransactionId): LogEntry = {
