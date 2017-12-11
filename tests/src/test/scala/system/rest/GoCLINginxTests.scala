2960c749a66922fab9119660d76fb533d6cf2aa6
David Grove <groved@us.ibm.com>
2017-12-11 15:15:58 -0500

    Make OpenWhisk CLI available only via redirect
    As was already done with the iOS starter app and the blackbox SDK,
    switch to making the cli binaries available via a redirect to the
    openwhisk-cli release page instead of copying cli binaries into
    the edge container.  As discussed in issue #2152 this eliminates
    the need for a custom OpenWhisk edge image for downstream deployments.
    
    Also update GoCLINginxTests to check for redirects instead of direct
    downloads and remove the cli role and related variable definitions
    from ansible.

--- tests/src/test/scala/system/rest/GoCLINginxTests.scala
+++ tests/src/test/scala/system/rest/GoCLINginxTests.scala
@@ -24,11 +24,8 @@ import org.scalatest.junit.JUnitRunner
 
 import com.jayway.restassured.RestAssured
 
-import spray.json._
-import DefaultJsonProtocol._
-
 /**
- * Basic tests of the download link for Go CLI binaries
+ * Basic tests of the download redirects for Go CLI binaries
  */
 @RunWith(classOf[JUnitRunner])
 class GoCLINginxTests extends FlatSpec with Matchers with RestUtil {
@@ -38,48 +35,26 @@ class GoCLINginxTests extends FlatSpec with Matchers with RestUtil {
   val Architectures = List("386", "amd64")
   val ServiceURL = getServiceURL()
 
-  it should s"respond to all files in root directory" in {
-    val response = RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli")
-    response.statusCode should be(200)
-    val responseString = response.body.asString
-    responseString should include("""<a href="content.json">content.json</a>""")
-    val responseJSON = RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli/content.json")
-    responseJSON.statusCode should be(200)
-    val cli = responseJSON.body.asString.parseJson.asJsObject
-      .fields("cli")
-      .convertTo[Map[String, Map[String, Map[String, String]]]]
-    cli.foreach {
-      case (os, arch) => responseString should include(s"""<a href="$os/">$os/</a>""")
-    }
+  it should s"check redirect for root directory request" in {
+    val response =
+      RestAssured.given().config(sslconfig).redirects().follow(false).get(s"$ServiceURL/$DownloadLinkGoCli")
+    response.statusCode should be(301)
+    response.header("Location") should include("https://github.com/apache/incubator-openwhisk-cli/releases")
+    println(response.header("Location"))
   }
 
-  it should "respond to all operating systems and architectures in HTML index" in {
-    val responseJSON = RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli/content.json")
-    responseJSON.statusCode should be(200)
-    val cli = responseJSON.body.asString.parseJson.asJsObject
-      .fields("cli")
-      .convertTo[Map[String, Map[String, Map[String, String]]]]
-    cli.foreach {
-      case (os, arch) =>
-        val response = RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli/$os")
-        response.statusCode should be(200)
-        val responseString = response.body.asString
-        arch.foreach {
-          case (arch, path) =>
-            if (arch != "default") {
-              responseString should include(s"""<a href="$arch/">$arch/</a>""")
-            }
-        }
-    }
-  }
-
-  it should "respond to the download paths in content.json" in {
-    val response = RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli/content.json")
-    response.statusCode should be(200)
-    val cli =
-      response.body.asString.parseJson.asJsObject.fields("cli").convertTo[Map[String, Map[String, Map[String, String]]]]
-    cli.values.flatMap(_.values).flatMap(_.values).foreach { path =>
-      RestAssured.given().config(sslconfig).get(s"$ServiceURL/$DownloadLinkGoCli/$path").statusCode should be(200)
+  it should "check redirect exists for all operating system and architecture pairs" in {
+    for (os <- OperatingSystems) {
+      for (arch <- Architectures) {
+        val response = RestAssured
+          .given()
+          .config(sslconfig)
+          .redirects()
+          .follow(false)
+          .get(s"$ServiceURL/$DownloadLinkGoCli/$os/$arch")
+        response.statusCode should be(301)
+        response.header("Location") should include(s"$os-$arch")
+      }
     }
   }
 }
