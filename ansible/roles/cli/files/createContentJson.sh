4f5ca8e4ee265bebcd7e6572b0136dab3dd9350e
Jonathan Springer <jonpspri@gmail.com>
2018-01-03 18:47:57 -0500

    Support CLI for s390x linux

--- ansible/roles/cli/files/createContentJson.sh
+++ ansible/roles/cli/files/createContentJson.sh
@@ -35,35 +35,45 @@ get_binary_path() {
 create_cli_packages() {
   local dirIndex="{\"cli\":{"
 
-  for platform in $platforms; do
-    dirIndex="$dirIndex\"$platform\":{"
+  #  I'm making a nasty assumption here that it's sorted by platforms
+  old_platform=''  # Not really necessary, but here to be explicit & transparent
+  for platform_arch in $platforms; do
 
-    for arch in $archs; do
-      comp_name=$(get_compressed_name $platform $arch $zip_name)
-      comp_path=$(get_binary_path $comp_name $platform $arch)
+    IFS="," read platform arch <<<"${platform_arch}"
 
-      if [ $arch = $default_arch ]; then
-          dirIndex="$dirIndex\"default\":{\"path\":\"$comp_path\"},";
+    #  Control-break processing for platform changes
+    if [ "$platform" != "$old_platform" ]; then
+      if [ "$old_platform" != "" ]; then
+        dirIndex="${dirIndex%','}" # Remove trailing comma
+        dirIndex+="},"             # And replace with end-brace comma
       fi
+      dirIndex+="\"$platform\":{"
+    fi
+    old_platform=$platform
 
-      dirIndex="$dirIndex\"$arch\":{\"path\":\"$comp_path\"},";
-    done
+    comp_name=$(get_compressed_name $platform $arch $zip_name)
+    comp_path=$(get_binary_path $comp_name $platform $arch)
 
-    dirIndex="$(echo $dirIndex | rev | cut -c2- | rev)"
-    dirIndex="$dirIndex},";
+    if [ $arch = $default_arch ]; then
+      dirIndex+="\"default\":{\"path\":\"$comp_path\"},";
+    fi
+
+    dirIndex+="\"$arch\":{\"path\":\"$comp_path\"},";
   done
 
-  dirIndex="$(echo $dirIndex | rev | cut -c2- | rev)"
-  dirIndex="$dirIndex}}"
+  #dirIndex="$(echo $dirIndex | rev | cut -c2- | rev)"
+  dirIndex="${dirIndex%','}"   # Remove trailing comma
+  dirIndex+="}}}"              # And replace with end-braces
 
   mkdir -p "${PATH_CONTENT_JSON}"
   echo $dirIndex > "${PATH_CONTENT_JSON}/content.json"
 };
 
-archs="386 amd64";
 default_arch="amd64"
 
 PATH_CONTENT_JSON=$1
+# 'shellcheck' pointed out that BINARY_TAG is unused.  I'm leaving it for now
+# because the future of content.json is in flux.
 BINARY_TAG=$2
 platforms=$3
 zip_name=$4
