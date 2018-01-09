2e301cd1ef3d43b958ce7e8b7b35dbed884d62e9
Rodric Rabbah <rabbah@us.ibm.com>
2018-01-09 14:38:57 -0500

    Remove 'wsk list' backing API. Update tests.

--- core/controller/src/main/scala/whisk/core/controller/Namespaces.scala
+++ core/controller/src/main/scala/whisk/core/controller/Namespaces.scala
@@ -17,38 +17,18 @@
 
 package whisk.core.controller
 
-import scala.concurrent.Future
-import scala.util.Failure
-import scala.util.Success
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives
-import akka.http.scaladsl.server.RequestContext
-import akka.http.scaladsl.server.RouteResult
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import spray.json.DefaultJsonProtocol._
 import whisk.common.TransactionId
-import whisk.core.entitlement.{Collection, Privilege, Resource}
-import whisk.core.entitlement.Privilege.READ
-import whisk.core.entity.EntityPath
+import whisk.core.entitlement.Collection
 import whisk.core.entity.Identity
-import whisk.core.entity.WhiskAction
-import whisk.core.entity.WhiskEntityQueries.listAllInNamespace
-import whisk.core.entity.WhiskPackage
-import whisk.core.entity.WhiskRule
-import whisk.core.entity.WhiskTrigger
-import whisk.core.entity.types.EntityStore
-import whisk.http.ErrorResponse.terminate
 
-trait WhiskNamespacesApi
-    extends Directives
-    with AuthenticatedRouteProvider
-    with BasicAuthorizedRouteProvider
-    with ReadOps {
+trait WhiskNamespacesApi extends Directives with AuthenticatedRouteProvider {
 
-  protected override val collection = Collection(Collection.NAMESPACES)
-
-  /** Database service to lookup entities in a namespace. */
-  protected val entityStore: EntityStore
+  protected val collection = Collection(Collection.NAMESPACES)
+  protected val collectionOps = pathEndOrSingleSlash & get
 
   /** JSON response formatter. */
   import RestApiCommons.jsonDefaultResponsePrinter
@@ -56,76 +36,13 @@ trait WhiskNamespacesApi
   /**
    * Rest API for managing namespaces. Defines all the routes handled by this API. They are:
    *
-   * GET  namespaces[/] -- gets namespaces for authenticated user
-   * GET  namespaces/_[/] -- gets all entities in implicit namespace
-   * GET  namespaces/namespace[/] -- gets all entities in explicit namespace
+   * GET  namespaces[/] -- gets namespace for authenticated user
    *
    * @param user the authenticated user for this route
    */
   override def routes(user: Identity)(implicit transid: TransactionId) = {
-    pathPrefix(collection.path) {
-      (collectionOps & requestMethod) { m =>
-        getNamespaces(user)
-      } ~ (entityOps & entityPrefix & pathEndOrSingleSlash & requestMethod) { (segment, m) =>
-        namespace(user, segment) { ns =>
-          val resource = Resource(ns, collection, None)
-          authorizeAndDispatch(m, user, resource)
-        }
-      }
+    (pathPrefix(collection.path) & collectionOps) {
+      complete(OK, List(user.namespace))
     }
   }
-
-  /**
-   * GET  / -- gets namespaces for authenticated user
-   * GET  /namespace -- gets all entities in namespace
-   *
-   * The namespace of the resource is derived from the authenticated user. The
-   * resource entity name, if it is defined, may be a different namespace.
-   */
-  protected override def dispatchOp(user: Identity, op: Privilege, resource: Resource)(
-    implicit transid: TransactionId) = {
-    resource.entity match {
-      case None if op == READ => getAllInNamespace(resource.namespace)
-      case _                  => reject // should not get here
-    }
-  }
-
-  /**
-   * Gets all entities in namespace.
-   *
-   * Responses are one of (Code, Message)
-   * - 200 Map [ String (collection name), List[EntitySummary] ] as JSON
-   * - 500 Internal Server Error
-   */
-  private def getAllInNamespace(namespace: EntityPath)(
-    implicit transid: TransactionId): RequestContext => Future[RouteResult] = {
-    onComplete(listAllInNamespace(entityStore, namespace.root, false)) {
-      case Success(entities) => {
-        complete(OK, Namespaces.emptyNamespace ++ entities)
-      }
-      case Failure(t) =>
-        logging.error(this, s"[GET] namespaces failed: ${t.getMessage}")
-        terminate(InternalServerError)
-    }
-  }
-
-  /**
-   * Gets namespaces for subject from entitlement service.
-   *
-   * Responses are one of (Code, Message)
-   * - 200 [ Namespaces (as String) ] as JSON
-   * - 401 Unauthorized
-   * - 500 Internal Server Error
-   */
-  private def getNamespaces(user: Identity)(implicit transid: TransactionId) = {
-    complete(OK, List(user.namespace))
-  }
-}
-
-object Namespaces {
-  val emptyNamespace = Map(
-    WhiskAction.collectionName -> List(),
-    WhiskPackage.collectionName -> List(),
-    WhiskRule.collectionName -> List(),
-    WhiskTrigger.collectionName -> List())
 }
