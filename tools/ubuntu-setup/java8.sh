f0d1d13e94b287ba33ba15605628cf7377463c32
sudswas <sudipto.biswas.dj@gmail.com>
2018-01-24 09:05:16 -0600

    Changes needed to support ppc64le
    This patch introduces an initial set of changes needed to support
    ppc64le as a platform on openwhisk. Some of the changes are probably
    not needed in long term once we get multi-arch images available.
    
    The changes are built on top of the PR:3133

--- tools/ubuntu-setup/java8.sh
+++ tools/ubuntu-setup/java8.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 set -x
+<<<<<<< HEAD
 
 if [ "$(lsb_release -cs)" == "trusty" ]; then
     sudo apt-get install -y software-properties-common python-software-properties
@@ -9,3 +10,17 @@ if [ "$(lsb_release -cs)" == "trusty" ]; then
 fi
 
 sudo apt-get install openjdk-8-jdk -y
+=======
+arch=`uname -m`
+if [ $arch == "ppc64le" ]
+then
+   sudo apt-get update -y
+   sudo apt-get install openjdk-8-jdk -y
+else
+   sudo apt-get install -y software-properties-common
+   sudo add-apt-repository -y ppa:webupd8team/java
+   sudo apt-get update -y
+   echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+   sudo apt-get install -y oracle-java8-installer
+fi
+>>>>>>> Changes needed to support ppc64le
