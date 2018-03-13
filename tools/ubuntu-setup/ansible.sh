ce913005e19a1e1908d435020c3c28127938cea3
Vadim Raskin <vadimr@de.ibm.com>
2018-03-13 10:34:46 +0100

    Update ansible version in readme

--- tools/ubuntu-setup/ansible.sh
+++ tools/ubuntu-setup/ansible.sh
@@ -25,7 +25,7 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y python-dev libffi-dev libssl-dev
 sudo pip install markupsafe
-sudo pip install ansible==2.3.0.0
+sudo pip install ansible==2.4.2.0
 sudo pip install docker==2.2.1
 sudo pip install jinja2==2.9.6
 sudo pip install httplib2==0.9.2
