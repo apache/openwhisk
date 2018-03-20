e371dfb4a5f44249b7f760b7b32370611f3570a8
Sven Lange-Last <sven.lange-last@de.ibm.com>
2018-03-20 16:14:27 +0100

    Add a CPU-friendly Node.js-based sleep action to be used in tests
    The new sleep action is meant to replace the existing test action `timeout.js` which uses a busy-wait loop that keeps one full CPU busy while waiting.

---
+++ tests/dat/actions/sleep.js
@@ -0,0 +1,40 @@
+/**
+ * Node.js based OpenWhisk action that sleeps for the specified number
+ * of milliseconds before returning. Uses a timer instead of a busy loop.
+ * The function actually sleeps slightly longer than requested.
+ *
+ * @param parm Object with Number property sleepTimeInMs
+ * @returns Object with String property msg describing how long the function slept
+ *          or an error object on failure
+ */
+function main(parm) {
+    if(!('sleepTimeInMs' in parm)) {
+        const result = { error: "Parameter 'sleepTimeInMs' not specified." }
+        console.error(result.error)
+        return result
+    }
+
+    if(!Number.isInteger(parm.sleepTimeInMs)) {
+        const result = { error: "Parameter 'sleepTimeInMs' must be an integer value." }
+        console.error(result.error)
+        return result
+    }
+
+    if((parm.sleepTimeInMs < 0) || !Number.isFinite(parm.sleepTimeInMs)) {
+        const result = { error: "Parameter 'sleepTimeInMs' must be finite, postive integer value." }
+        console.error(result.error)
+        return result
+    }
+
+    console.log("Specified sleep time is " + parm.sleepTimeInMs + " ms.")
+
+    return new Promise(function(resolve, reject) {
+        const timeBeforeSleep = new Date()
+        setTimeout(function () {
+            const actualSleepTimeInMs = new Date() - timeBeforeSleep
+            const result = { msg: "Terminated successfully after around " + actualSleepTimeInMs + " ms." }
+            console.log(result.msg)
+            resolve(result)
+        }, parm.sleepTimeInMs)
+    })
+}
