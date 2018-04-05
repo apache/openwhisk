d8a7414aa99c7300706e2b38853b5fcaa5d61f3a
Christian Bickel <cbickel@de.ibm.com>
2018-04-05 10:36:23 +0200

    Add build matrix to execute perormance tests.

--- performance/preparation/deploy.sh
+++ performance/preparation/deploy.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
 set -e
-currentDir="$(cd "$(dirname "$0")"; pwd)"
+SCRIPTDIR="$(cd "$(dirname "$0")"; pwd)"
+ROOTDIR="$SCRIPTDIR/../.."
 
-# common docker setup
-sudo gpasswd -a travis docker
-sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --storage-driver=overlay --userns-remap=default"'\'' > /etc/default/docker'
-sudo service docker restart
+# Build Openwhisk
+cd $ROOTDIR
+TERM=dumb ./gradlew distDocker -PdockerImagePrefix=testing $GRADLE_PROJS_SKIP
 
-# install ansible
-pip install --user ansible==2.4.2.0
-
-cd $currentDir/../../ansible
-ANSIBLE_CMD="ansible-playbook -i environments/local -e docker_image_prefix=openwhisk -e docker_registry=docker.io/ -e limit_invocations_per_minute=999999 -e limit_invocations_concurrent=999999 -e limit_invocations_concurrent_system=999999"
+# Deploy Openwhisk
+cd $ROOTDIR/ansible
+ANSIBLE_CMD="$ANSIBLE_CMD -e limit_invocations_per_minute=999999 -e limit_invocations_concurrent=999999 -e limit_invocations_concurrent_system=999999 -e controller_client_auth=false"
 
 $ANSIBLE_CMD setup.yml
 $ANSIBLE_CMD prereq.yml
