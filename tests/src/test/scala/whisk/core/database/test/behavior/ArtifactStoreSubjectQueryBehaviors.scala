033bec5122b0e963037d2f75213f9faa870dcd96
Chetan Mehrotra <chetanm@apache.org>
2018-04-06 14:25:46 +0530

    Test to validate subject limits support
    Validates that implicit join from subject record to limits is working

--- tests/src/test/scala/whisk/core/database/test/behavior/ArtifactStoreSubjectQueryBehaviors.scala
+++ tests/src/test/scala/whisk/core/database/test/behavior/ArtifactStoreSubjectQueryBehaviors.scala
@@ -42,4 +42,30 @@ trait ArtifactStoreSubjectQueryBehaviors extends ArtifactStoreBehaviorBase {
     s2.subject shouldBe subs(1).subject
   }
 
+  it should "find subject by namespace with limits" in {
+    implicit val tid: TransactionId = transid()
+    val ak1 = AuthKey()
+    val ak2 = AuthKey()
+    val name1 = aname()
+    val name2 = aname()
+    val subs = Array(
+      WhiskAuth(Subject(), Set(WhiskNamespace(name1, ak1))),
+      WhiskAuth(Subject(), Set(WhiskNamespace(name2, ak2))))
+    subs foreach (put(authStore, _))
+
+    val limits = UserLimits(invocationsPerMinute = Some(7), firesPerMinute = Some(31))
+    put(authStore, new LimitEntity(name1, limits))
+
+    val i = Identity.get(authStore, name1).futureValue
+    i.subject shouldBe subs(0).subject
+    i.limits shouldBe limits
+  }
+
+  private class LimitEntity(name: EntityName, limits: UserLimits) extends WhiskAuth(Subject(), Set.empty) {
+    override def docid = DocId(s"${name.name}/limits")
+
+    //There is no api to write limits. So piggy back on WhiskAuth but replace auth json
+    //with limits!
+    override def toJson = UserLimits.serdes.write(limits).asJsObject
+  }
 }
