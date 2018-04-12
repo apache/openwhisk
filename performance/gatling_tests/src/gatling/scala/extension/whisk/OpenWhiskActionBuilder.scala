f0c4c0004e15bed7f17f958309d31a87e7c971db
Christian Bickel <cbickel@de.ibm.com>
2018-04-12 17:46:13 +0200

    Remove functionality that is not used yet, because it has to be reworked.

--- performance/gatling_tests/src/gatling/scala/extension/whisk/OpenWhiskActionBuilder.scala
+++ performance/gatling_tests/src/gatling/scala/extension/whisk/OpenWhiskActionBuilder.scala
@@ -22,61 +22,13 @@ import io.gatling.core.action.Action
 import io.gatling.core.action.builder.ActionBuilder
 import io.gatling.core.session.Expression
 import io.gatling.core.structure.ScenarioContext
-import io.gatling.core.util.Resource
-import io.gatling.http.Predef._
 import io.gatling.http.request.builder.{Http, HttpRequestBuilder}
-import spray.json.DefaultJsonProtocol._
-import spray.json._
-
-import scala.io.Source
-import scala.language.implicitConversions
 
 case class OpenWhiskActionBuilderBase(requestName: Expression[String]) {
 
   implicit private val http = new Http(requestName)
 
   def info() = OpenWhiskActionBuilder(http.get("/api/v1"))
-  def authenticate(uuid: String, key: String) = OpenWhiskActionBuilderWithNamespace(uuid, key)
-}
-
-case class OpenWhiskActionBuilderWithNamespace(private val uuid: String,
-                                               private val key: String,
-                                               private val namespace: String = "_")(implicit private val http: Http) {
-
-  def namespace(namespace: String) = OpenWhiskActionBuilderWithNamespace(uuid, key, namespace)
-  def actionInvoke(actionName: String, blocking: Boolean = false) = {
-    OpenWhiskActionBuilderActionInvoke(
-      http
-        .post(s"/api/v1/namespaces/$namespace/actions/$actionName?blocking=${blocking.toString}")
-        .basicAuth(uuid, key)
-        .check(status.is(if (blocking) 200 else 202)))
-  }
-  def actionCreate(actionName: String, fileName: String, kind: String = "nodejs:default") =
-    OpenWhiskActionBuilder(
-      http
-        .put(s"/api/v1/namespaces/$namespace/actions/$actionName")
-        .basicAuth(uuid, key)
-        .body(createActionCreateBody(fileName, kind)))
-  def actionDelete(actionName: String) =
-    OpenWhiskActionBuilder(http.delete(s"/api/v1/namespaces/$namespace/actions/$actionName").basicAuth(uuid, key))
-
-  private def createActionCreateBody(fileName: String, kind: String) = {
-    val code = Source.fromFile(Resource.feeder(fileName).get.file).mkString
-    val json = JsObject("exec" -> JsObject("kind" -> JsString(kind), "code" -> JsString(code)))
-    StringBody(json.compactPrint)
-  }
-}
-
-case class OpenWhiskActionBuilderActionInvoke(http: HttpRequestBuilder, parameters: Map[String, String] = Map()) {
-
-  def withParameters(key: String, value: String) =
-    OpenWhiskActionBuilderActionInvoke(http, parameters ++ Map((key, value)))
-  def build = OpenWhiskActionBuilder(http.body(StringBody(parameters.toJson.compactPrint)))
-}
-
-object OpenWhiskActionBuilderActionInvoke {
-  implicit def toOpenWhiskActionBuilder(builder: OpenWhiskActionBuilderActionInvoke): OpenWhiskActionBuilder =
-    builder.build
 }
 
 case class OpenWhiskActionBuilder(http: HttpRequestBuilder) extends ActionBuilder {
