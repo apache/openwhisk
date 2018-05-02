056c118ec244d997622adbd2d93a79e51678ff2e
Ying Chun Guo <guoyingc@cn.ibm.com>
2018-05-02 22:48:56 +0800

    add short licnese header to test data files (#3596)

--- tests/dat/actions/memoryWithGC.js
+++ tests/dat/actions/memoryWithGC.js
@@ -1,3 +1,6 @@
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
+
 function eat(memoryMB) {
     var bytes = 1*1024*1024*memoryMB;
     var buffer = new Buffer.alloc(bytes, 'a');
