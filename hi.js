049adebcda40c759aac85852a14b82ed8ac19ae2
Your Name <you@example.com>
2018-06-20 15:52:18 +0200

    Change the load balancer to reduce invoker activation queueing #3755

--- hi.js
+++
@@ -1,7 +0,0 @@
-function main() {
-	var stop = new Date().getTime();
-    while(new Date().getTime() < stop + 20000) {
-        ;
-    }
-    return {payload: 'Helo Hamada'};
-}
--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Basic while loop
-counter=1
-while [ $counter -le 30 ]
-do
-wsk action invoke hi
-counter=`expr $counter + 1`
-done
