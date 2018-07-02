f3a54522e3bed66b1fd5fd7343b73f43ae9123fa
Chetan Mehrotra <chetanm@apache.org>
2018-07-02 11:44:50 +0530

    Reformat

--- tools/dev/src/main/groovy/CategoryManager.groovy
+++ tools/dev/src/main/groovy/CategoryManager.groovy
@@ -1,5 +1,3 @@
-import java.util.function.Predicate
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -16,6 +14,8 @@ import java.util.function.Predicate
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+import java.util.function.Predicate
 
 class CategoryManager {
     def categories = process([
