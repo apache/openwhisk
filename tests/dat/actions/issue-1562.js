d2926f7895544032778d4c6043736c8cc47f70c7
Rodric Rabbah <rodric@gmail.com>
2018-07-08 22:23:04 -0400

    Remove unused file.

--- tests/dat/actions/issue-1562.js
+++
@@ -1,9 +0,0 @@
-// Licensed to the Apache Software Foundation (ASF) under one or more contributor
-// license agreements; and to You under the Apache License, Version 2.0.
-
-// We expect this action to always result in errored activations.
-function main(args) {
-    return new Promise((resolve, reject) => {
-        reject();
-    });
-}
