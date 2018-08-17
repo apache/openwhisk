f5c9a984039eca7226f7b24b422ab10fbd754a3a
rahultripathi <rtripath@adobe.com>
2018-08-17 20:40:48 +0530

    Deleted extra code

--- tests/functional/karate_tests/getHTTP.js
+++
@@ -1,7 +0,0 @@
-function main({name:name='Serverless API'}) {
-    return {
-      body: {payload:`Hello world ${name}`},
-      statusCode: 200,
-      headers:{ 'Content-Type': 'application/json'}
-    };
-}
--- tests/functional/karate_tests/src/test/java/com/karate/openwhisk/functions/greetings.js
+++
@@ -1,7 +0,0 @@
-function() {
-    // Licensed to the Apache Software Foundation (ASF) under one or more contributor
-    // license agreements; and to You under the Apache License, Version 2.0.
-    var scriptcodeWithParam ="function main(params) {\n  console.log('params:', params);\n  var name = params.name || 'stranger';\n  var place = params.place || 'somewhere';\n  return {msg:  'Hello, ' + name + ' from ' + place + '!'};\n}";
-    return scriptcodeWithParam;
-}
-//sample greeting method
