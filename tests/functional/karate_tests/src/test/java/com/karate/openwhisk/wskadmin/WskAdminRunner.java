b143faf6fcc9dbfc73b668c835cc04eaad3b7b2e
rahultripathi <rtripath@adobe.com>
2018-08-17 16:26:48 +0530

    Refactored Code For Demo

--- tests/functional/karate_tests/src/test/java/com/karate/openwhisk/wskadmin/WskAdminRunner.java
+++ tests/functional/karate_tests/src/test/java/com/karate/openwhisk/wskadmin/WskAdminRunner.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-// Copyright 2017-2018 Adobe.
 package com.karate.openwhisk.wskadmin;
 
 import static org.junit.Assert.assertTrue;
