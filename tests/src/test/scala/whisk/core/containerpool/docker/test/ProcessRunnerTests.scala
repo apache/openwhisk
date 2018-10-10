399b5e61069166f9fe530012f19d68a64a3374df
Chetan Mehrotra <chetanm@apache.org>
2018-10-10 23:40:14 +0530

    Resolve ambiguity between gloabl and local execution context
    https://stackoverflow.com/questions/40442717/implicit-executioncontext-priority-in-scala-2-12

--- tests/src/test/scala/whisk/core/containerpool/docker/test/ProcessRunnerTests.scala
+++ tests/src/test/scala/whisk/core/containerpool/docker/test/ProcessRunnerTests.scala
@@ -25,7 +25,6 @@ import org.junit.runner.RunWith
 import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
 
-import scala.concurrent.ExecutionContext.Implicits.global
 import whisk.core.containerpool.docker.ProcessRunner
 
 import scala.concurrent.ExecutionContext
