0b8a62a7eef9e65cb76d05b7c4fe9119d9bacc4c
Carlos Santana <csantanapr@apache.org>
2018-10-30 19:59:55 -0400

    removed sudo for travis from docker group

--- tools/travis/setup.sh
+++ tools/travis/setup.sh
@@ -30,7 +30,6 @@ function retry() {
   fi
 }
 
-#sudo gpasswd -a travis docker
 sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --storage-driver=overlay --userns-remap=default"'\'' > /etc/default/docker'
 
 # Docker
