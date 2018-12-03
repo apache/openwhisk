e3c86b3ee4c79a8bb4a5d62611bfc9720f870927
Chetan Mehrotra <chetanm@apache.org>
2018-12-03 15:34:43 +0530

    Format

--- tests/dat/actions/memoryWithGC.js
+++ tests/dat/actions/memoryWithGC.js
@@ -2,13 +2,13 @@
 // license agreements; and to You under the Apache License, Version 2.0.
 
 function eat(memoryMB) {
-    var bytes = 1*1024*1024*memoryMB;
+    var bytes = 1024 * 1024 * memoryMB;
     var data = "abcdefghijklmnopqrstuvwxyz";
     var arr = [];
-    while(arr.length * data.length < bytes) {
+    while (arr.length * data.length < bytes) {
         arr.push(data)
     }
-    console.log('done.');
+    console.log('done.' + arr.length);
 }
 
 function main(msg) {
