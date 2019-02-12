2d33df5c6670ab70d329a8d0e31b082cc86fbf28
rahulqelfo <er.rahultr@yahoo.com>
2019-02-12 15:13:19 -0500

    created Base Class for Karate Cucumber reports

--- tests/functional/karate_tests/src/test/java/org/apache/openwhisk/utils/GenerateReports.java
+++ tests/functional/karate_tests/src/test/java/org/apache/openwhisk/utils/GenerateReports.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.apache.openwhisk.smoketests;
+package org.apache.openwhisk.utils;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -32,18 +32,18 @@ import com.intuit.karate.cucumber.KarateStats;
 import cucumber.api.CucumberOptions;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
-import org.apache.openwhisk.utils.*;
 
-@CucumberOptions(tags = {"~@ignore","~@driver","~@reliability","~@resiliency","~@concurrent","~@wskfunctions"})
-public class SmokeTestRunner extends GenerateReports {
-    @Test
-    public void testwskFunctions() {
+public class GenerateReports {
 
-        String karateOutputPath = "target/surefire-reports";
-        KarateStats stats = CucumberRunner.parallel(getClass(), 1, karateOutputPath);
-        GenerateReports reports = new GenerateReports();
-        reports.generateReport(karateOutputPath);
-        assertTrue("there are scenario failures", stats.getFailCount() == 0);
+    public void generateReport(String karateOutputPath) {
+
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "openwhisk");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+        String dir = System.getProperty("user.dir");
+        //  System.out.print("Enter the file to use: "+dir/target/cucumber-html-reports/);
     }
-
 }
