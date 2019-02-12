55a59c22b3513f5c801edd82d82d7e16026842c4
rahulqelfo <er.rahultr@yahoo.com>
2019-02-12 04:23:16 -0500

    fixed issues based on reviews and scanCode and flake8 issues

--- tests/functional/karate_tests/src/test/java/karate-config.js
+++ tests/functional/karate_tests/src/test/java/karate-config.js
@@ -1,11 +1,10 @@
 function() {
-    // Licensed to the Apache Software Foundation (ASF) under one or more contributor
-    // license agreements; and to You under the Apache License, Version 2.0.
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
     var env = karate.env; // get system property 'karate.env'
     var adminauth = karate.properties['adminauth'];
     var adminbaseurl = karate.properties['adminbaseurl'];
     var baseurl = karate.properties['baseurl'];
-
     karate.log('karate.env system property was:', env);
 
     if (!adminauth) {
--- tests/functional/karate_tests/src/test/java/org/apache/openwhisk/functions/hello-world.js
+++ tests/functional/karate_tests/src/test/java/org/apache/openwhisk/functions/hello-world.js
@@ -1,6 +1,6 @@
 function() {
-    // Licensed to the Apache Software Foundation (ASF) under one or more contributor
-    // license agreements; and to You under the Apache License, Version 2.0.
+// Licensed to the Apache Software Foundation (ASF) under one or more contributor
+// license agreements; and to You under the Apache License, Version 2.0.
 var scriptcode ="function main(params) {\n    console.log('hello stdout');\n    console.error('hello stderr');\n    var name = params.name || \"World\";\n    return {payload: \"Hello, \" + name + \"!\"};\n}\n";
 return scriptcode;
 }
