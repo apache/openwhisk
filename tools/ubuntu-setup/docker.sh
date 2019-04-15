63e957ad3bc2d5ea251025fbbfb6a80e1c763cb5
Dominic Kim <style9595@gmail.com>
2019-04-15 16:13:39 +0900

    Update docker version in the controller and guides

--- tools/ubuntu-setup/docker.sh
+++ tools/ubuntu-setup/docker.sh
@@ -19,24 +19,27 @@
 set -e
 set -x
 
-sudo apt-get -y install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-sudo sh -c "echo deb https://apt.dockerproject.org/repo ubuntu-trusty main  > /etc/apt/sources.list.d/docker.list"
-sudo apt-get -y update
+sudo sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
 
-sudo apt-get purge lxc-docker
-sudo apt-cache policy docker-engine
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo add-apt-repository \
+    "deb [arch=$(uname -m | sed -e 's/x86_64/amd64/g')] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get -y update
 
 # AUFS
 # Use '-virtual' package to support docker tests of the script and still run under Vagrant
 sudo apt-get --no-install-recommends -y install linux-image-extra-virtual
 
 # DOCKER
-sudo apt-get install -y --force-yes docker-engine=1.12.0-0~trusty
-sudo apt-mark hold docker-engine
+sudo apt-get install -y docker-ce=18.06.3~ce~3-0~ubuntu containerd.io
+sudo apt-mark hold docker-ce
 
 # enable (security - use 127.0.0.1)
-sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --storage-driver=aufs"'\'' >> /etc/default/docker'
+sudo -E bash -c 'echo '\''DOCKER_OPTS="-H unix:///var/run/docker.sock --storage-driver=aufs"'\'' >> /etc/default/docker'
 sudo gpasswd -a "$(whoami)" docker
 
 
