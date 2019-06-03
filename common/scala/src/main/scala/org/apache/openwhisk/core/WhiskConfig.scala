4c3068853355c92b70ceffc4ca9caf2f035b1747
Rodric Rabbah <rodric@gmail.com>
2019-06-03 10:52:17 -0400

    Remove default port on api host.

--- common/scala/src/main/scala/org/apache/openwhisk/core/WhiskConfig.scala
+++ common/scala/src/main/scala/org/apache/openwhisk/core/WhiskConfig.scala
@@ -19,19 +19,22 @@ package org.apache.openwhisk.core
 
 import java.io.File
 
-import scala.io.Source
+import akka.http.scaladsl.model.Uri.normalize
 import org.apache.openwhisk.common.{Config, Logging}
+
+import scala.io.Source
+import scala.util.Try
 
 /**
  * A set of properties which might be needed to run a whisk microservice implemented
  * in scala.
  *
  * @param requiredProperties a Map whose keys define properties that must be bound to
- * a value, and whose values are default values. A null value in the Map means there is
- * no default value specified, so it must appear in the properties file.
+ *                           a value, and whose values are default values. A null value in the Map means there is
+ *                           no default value specified, so it must appear in the properties file.
  * @param optionalProperties a set of optional properties (which may not be defined).
- * @param propertiesFile a File object, the whisk.properties file, which if given contains the property values.
- * @param env an optional environment to initialize from.
+ * @param propertiesFile     a File object, the whisk.properties file, which if given contains the property values.
+ * @param env                an optional environment to initialize from.
  */
 class WhiskConfig(requiredProperties: Map[String, String],
                   optionalProperties: Set[String] = Set.empty,
@@ -54,8 +57,11 @@ class WhiskConfig(requiredProperties: Map[String, String],
   val dockerEndpoint = this(WhiskConfig.dockerEndpoint)
   val dockerPort = this(WhiskConfig.dockerPort)
 
-  val wskApiHost = this(WhiskConfig.wskApiProtocol) + "://" + this(WhiskConfig.wskApiHostname) + ":" + this(
-    WhiskConfig.wskApiPort)
+  val wskApiHost: String = Try(
+    normalize(
+      s"${this(WhiskConfig.wskApiProtocol)}://${this(WhiskConfig.wskApiHostname)}:${this(WhiskConfig.wskApiPort)}"))
+    .getOrElse("")
+
   val controllerBlackboxFraction = this.getAsDouble(WhiskConfig.controllerBlackboxFraction, 0.10)
   val controllerInstances = this(WhiskConfig.controllerInstances)
 
