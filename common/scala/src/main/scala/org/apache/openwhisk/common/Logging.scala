adobe-apiplatform/incubator-openwhisk

added metrics for resource error, rescheduled activation, number nodes, total memory, max memory

common/scala/src/main/scala/org/apache/openwhisk/common/Logging.scala
@@ -324,6 +324,8 @@ object LoggingMarkers {
   private val kafka = "kafka"
   private val loadbalancer = "loadbalancer"
   private val containerClient = "containerClient"
+  private val containerPool = "containerPool"
+  private val clusterResourceManager = "clusterResources"
 
   /*
    * Controller related markers
@@ -395,6 +397,16 @@ object LoggingMarkers {
       MeasurementUnit.none)
   val CONTAINER_CLIENT_RETRIES =
     LogMarkerToken(containerClient, "retries", count)(MeasurementUnit.none)
+  val CONTAINER_POOL_RESOURCE_ERROR =
+    LogMarkerToken(containerPool, "resourceError", count)(MeasurementUnit.none)
+  val CONTAINER_POOL_RESCHEDULED_ACTIVATION =
+    LogMarkerToken(containerPool, "rescheduledActivation", count)(MeasurementUnit.none)
+  val CLUSTER_RESOURCES_TOTAL_MEM =
+    LogMarkerToken(clusterResourceManager, "totalMemory", count)(MeasurementUnit.none)
+  val CLUSTER_RESOURCES_MAX_MEM =
+    LogMarkerToken(clusterResourceManager, "maxMemory", count)(MeasurementUnit.none)
+  val CLUSTER_RESOURCES_NODE_COUNT =
+    LogMarkerToken(clusterResourceManager, "nodes", count)(MeasurementUnit.none)
 
   val INVOKER_TOTALMEM_BLACKBOX = LogMarkerToken(loadbalancer, "totalCapacityBlackBox", count)(MeasurementUnit.none)
   val INVOKER_TOTALMEM_MANAGED = LogMarkerToken(loadbalancer, "totalCapacityManaged", count)(MeasurementUnit.none)

apache/openwhisk

common/scala/src/main/scala/org/apache/openwhisk/common/Logging.scala
@@ -350,6 +350,8 @@ object LoggingMarkers {
   private val kafka = "kafka"
   private val loadbalancer = "loadbalancer"
   private val containerClient = "containerClient"
+  private val containerPool = "containerPool"
+  private val clusterResourceManager = "clusterResources"
 
   /*
    * Controller related markers
@@ -445,6 +447,16 @@ object LoggingMarkers {
       MeasurementUnit.none)
   val CONTAINER_CLIENT_RETRIES =
     LogMarkerToken(containerClient, "retries", counter)(MeasurementUnit.none)
+  val CONTAINER_POOL_RESOURCE_ERROR =
+    LogMarkerToken(containerPool, "resourceError", counter)(MeasurementUnit.none)
+  val CONTAINER_POOL_RESCHEDULED_ACTIVATION =
+    LogMarkerToken(containerPool, "rescheduledActivation", counter)(MeasurementUnit.none)
+  val CLUSTER_RESOURCES_TOTAL_MEM =
+    LogMarkerToken(clusterResourceManager, "totalMemory", counter)(MeasurementUnit.none)
+  val CLUSTER_RESOURCES_MAX_MEM =
+    LogMarkerToken(clusterResourceManager, "maxMemory", counter)(MeasurementUnit.none)
+  val CLUSTER_RESOURCES_NODE_COUNT =
+    LogMarkerToken(clusterResourceManager, "nodes", counter)(MeasurementUnit.none)
 
   val INVOKER_TOTALMEM_BLACKBOX = LogMarkerToken(loadbalancer, "totalCapacityBlackBox", counter)(MeasurementUnit.none)
   val INVOKER_TOTALMEM_MANAGED = LogMarkerToken(loadbalancer, "totalCapacityManaged", counter)(MeasurementUnit.none)