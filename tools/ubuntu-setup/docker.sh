c11ef742ac11d9835db17254926ca032e58a8f22
Vincent Hou <shou@us.ibm.com>
2019-07-05 15:49:01 -0400

    Verify the PR builds

--- tools/ubuntu-setup/docker.sh
+++ tools/ubuntu-setup/docker.sh
@@ -48,3 +48,16 @@ sudo service docker restart
 # do not run this command without a vagrant reload during provisioning
 # it gives an error that docker is not up (which the reload fixes).
 # sudo docker version
+
+
+
+sudo apt-get  install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=$(uname -m | sed -e 's/x86_64/amd64/g')] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get  update
+sudo apt-get purge lxc-docker || /bin/true
+sudo apt-cache policy docker-engine
+sudo -E bash -c 'echo '\''DOCKER_OPTS="-H tcp://0.0.0.0:4243 -H unix:///var/run/docker.sock --storage-driver=aufs"'\'' >> /etc/default/docker'
+sudo apt-get install docker.io
+
