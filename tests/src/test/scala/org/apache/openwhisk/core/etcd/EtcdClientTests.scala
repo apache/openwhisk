845ccff76e4cbcf4e6bf84adf5ca23b2f2cf12dc
KeonHee <kimkh6554@gmail.com>
2019-07-10 17:52:16 +0900

    Add long license notice

--- tests/src/test/scala/org/apache/openwhisk/core/etcd/EtcdClientTests.scala
+++ tests/src/test/scala/org/apache/openwhisk/core/etcd/EtcdClientTests.scala
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.openwhisk.core.etcd
 
 import java.nio.charset.StandardCharsets
@@ -77,7 +93,7 @@ class EtcdClientTests
 
   "Etcd KV client" should "be able to put, get and delete" in {
     val key = "openwhisk"
-    val value = "lambda"
+    val value = "openwhisk"
 
     // Create an entry
     noException should be thrownBy etcd.put(key, value).futureValue(timeout)
