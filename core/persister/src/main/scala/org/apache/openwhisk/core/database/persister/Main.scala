e8c71f74537d52010fbd86a31d3d7628c7b7f457
Chetan Mehrotra <chetanm@apache.org>
2019-08-29 16:11:29 +0530

    More skeleton code

---
+++ core/persister/src/main/scala/org/apache/openwhisk/core/database/persister/Main.scala
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.openwhisk.core.database.persister
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import kamon.Kamon
+import org.apache.openwhisk.common.ConfigMXBean
+import org.apache.openwhisk.http.{BasicHttpService, BasicRasService}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    implicit val system: ActorSystem = ActorSystem("persister-actor-system")
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    ConfigMXBean.register()
+    Kamon.loadReportersFromConfig()
+    val config = PersisterConfig(system.settings.config)
+    val port = config.serviceConfig.port
+    BasicHttpService.startHttpService(new BasicRasService {}.route, port, None)
+    Persister.start(config)
+  }
+}
