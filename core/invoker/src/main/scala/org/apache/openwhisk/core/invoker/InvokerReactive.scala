40be9d8478f8102857a7d098af465df134a322f3
Rodric Rabbah <rodric@gmail.com>
2019-09-19 07:17:37 -0700

    If we were to remove the activation result as a parameter from the ack call.

--- core/invoker/src/main/scala/org/apache/openwhisk/core/invoker/InvokerReactive.scala
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/invoker/InvokerReactive.scala
@@ -164,9 +164,13 @@ class InvokerReactive(
 
     // UserMetrics are sent, when the slot is free again. This ensures, that all metrics are sent.
     if (UserEvents.enabled && acknowledegment.isSlotFree.nonEmpty) {
-      EventMessage.from(activationResult, s"invoker${instance.instance}", userId) match {
-        case Success(msg) => UserEvents.send(producer, msg)
-        case Failure(t)   => logging.error(this, s"activation event was not sent: $t")
+      acknowledegment.result.foreach {
+        case Right(activationResult) =>
+          EventMessage.from(activationResult, s"invoker${instance.instance}", userId) match {
+            case Success(msg) => UserEvents.send(producer, msg)
+            case Failure(t)   => logging.error(this, s"activation event was not sent: $t")
+          }
+        case _ => // all acknowledegment messages should have a result
       }
     }
 
