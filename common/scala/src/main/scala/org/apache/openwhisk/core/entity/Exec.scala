5f58c3510720f0dc20a1f4699808d305a7399ab5
Ali Tariq <alitariq@Alis-MacBook-Pro.local>
2019-10-28 19:09:07 -0600

    Added more details for unsupported runtimes exception

--- common/scala/src/main/scala/org/apache/openwhisk/core/entity/Exec.scala
+++ common/scala/src/main/scala/org/apache/openwhisk/core/entity/Exec.scala
@@ -323,7 +323,8 @@ object Exec extends ArgNormalizer[Exec] with DefaultJsonProtocol {
           // map "default" virtual runtime versions to the currently blessed actual runtime version
           val manifest = execManifests.resolveDefaultRuntime(kind) match {
             case Some(k) => k
-            case None    => throw new DeserializationException(s"kind '$kind' not in $runtimes")
+            case None    => throw new DeserializationException(
+              s"the specified runtime '$kind' is not supported by this platform. Valid values are: ${runtimes.mkString("'", "', '", "'")}")
           }
 
           manifest.attached
