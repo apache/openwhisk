2cdca7435d01074b556b100beba1bb1a7628d6a0
Ali Tariq <alitariq@Alis-MacBook-Pro.local>
2019-10-29 01:00:18 -0600

    redo

--- common/scala/src/main/scala/org/apache/openwhisk/core/entity/Exec.scala
+++ common/scala/src/main/scala/org/apache/openwhisk/core/entity/Exec.scala
@@ -324,7 +324,9 @@ object Exec extends ArgNormalizer[Exec] with DefaultJsonProtocol {
           val manifest = execManifests.resolveDefaultRuntime(kind) match {
             case Some(k) => k
             case None =>
-              throw new DeserializationException(s"the specified runtime '$kind' is not supported by this platform. Valid values are: ${runtimes.mkString("'", "', '", "'")}")
+              throw new DeserializationException(
+                s"the specified runtime '$kind' is not supported by this platform. Valid values are: ${runtimes
+                  .mkString("'", "', '", "'")}")
           }
 
           manifest.attached
