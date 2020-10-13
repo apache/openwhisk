a7a797b8369972b1e38e70f6ddf6089ba215e8d2
Nitika Agarwal <54163056+nitikagarw@users.noreply.github.com>
2020-10-13 18:25:25 +0530

    Controller should not return a payload for created and deleted entities (#4968)
    * Controller should not return a payload for created and deleted entities
    
    * Add Feature Flag support
    
    * Revert test cases changes
    
    * Introduce feature flag at all relevant places

--- core/controller/src/main/scala/org/apache/openwhisk/core/controller/ApiUtils.scala
+++ core/controller/src/main/scala/org/apache/openwhisk/core/controller/ApiUtils.scala
@@ -29,11 +29,13 @@ import akka.http.scaladsl.model.StatusCodes.Conflict
 import akka.http.scaladsl.model.StatusCodes.InternalServerError
 import akka.http.scaladsl.model.StatusCodes.NotFound
 import akka.http.scaladsl.model.StatusCodes.OK
+import akka.http.scaladsl.model.StatusCodes.NoContent
 import akka.http.scaladsl.server.{Directives, RequestContext, RouteResult}
 import spray.json.DefaultJsonProtocol._
 import spray.json.{JsObject, JsValue, RootJsonFormat}
 import org.apache.openwhisk.common.Logging
 import org.apache.openwhisk.common.TransactionId
+import org.apache.openwhisk.core.FeatureFlags
 import org.apache.openwhisk.core.controller.PostProcess.PostProcessEntity
 import org.apache.openwhisk.core.database._
 import org.apache.openwhisk.core.entity.{ActivationId, ActivationLogs, DocId, WhiskActivation, WhiskDocument}
@@ -341,7 +343,8 @@ trait WriteOps extends Directives {
     }) {
       case Success(entity) =>
         logging.debug(this, s"[PUT] entity success")
-        postProcess map { _(entity) } getOrElse complete(OK, entity)
+        if (FeatureFlags.requireResponsePayload) postProcess map { _(entity) } getOrElse complete(OK, entity)
+        else postProcess map { _(entity) } getOrElse complete(OK)
       case Failure(IdentityPut(a)) =>
         logging.debug(this, s"[PUT] entity exists, not overwritten")
         complete(OK, a)
@@ -399,7 +402,8 @@ trait WriteOps extends Directives {
     }) {
       case Success(entity) =>
         logging.debug(this, s"[DEL] entity success")
-        postProcess map { _(entity) } getOrElse complete(OK, entity)
+        if (FeatureFlags.requireResponsePayload) postProcess map { _(entity) } getOrElse complete(OK, entity)
+        else postProcess map { _(entity) } getOrElse complete(NoContent)
       case Failure(t: NoDocumentException) =>
         logging.debug(this, s"[DEL] entity does not exist")
         terminate(NotFound)
