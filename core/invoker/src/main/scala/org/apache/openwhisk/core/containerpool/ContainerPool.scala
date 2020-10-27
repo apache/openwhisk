739755aef8cd41e9d70bb83dfef6769303051640
Bin Wang <62220669+georgianfire@users.noreply.github.com>
2020-10-27 06:15:28 -0400

    Remove unused trait and case class (#5011)
    The trait `WorkerState` and case class `WorkerData` are no longer used
    anywhere. Keeping them in the code base makes no sense.

--- core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/ContainerPool.scala
+++ core/invoker/src/main/scala/org/apache/openwhisk/core/containerpool/ContainerPool.scala
@@ -29,13 +29,7 @@ import scala.collection.immutable
 import scala.concurrent.duration._
 import scala.util.{Random, Try}
 
-sealed trait WorkerState
-case object Busy extends WorkerState
-case object Free extends WorkerState
-
 case class ColdStartKey(kind: String, memory: ByteSize)
-
-case class WorkerData(data: ContainerData, state: WorkerState)
 
 case object EmitMetrics
 
