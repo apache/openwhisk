bb7192e90a046c5aaad9785fdd4ec6591ab78909
David Grove <groved@us.ibm.com>
2021-05-17 16:50:29 -0400

    prefetch openwhisk/example to avoid timeout during test execution

--- tools/travis/runStandaloneTests.sh
+++ tools/travis/runStandaloneTests.sh
@@ -47,6 +47,7 @@ kubectl config set-context --current --namespace=default
 # This is required because it is timed out to pull the image during the test.
 docker pull openwhisk/action-nodejs-v14:nightly
 docker pull openwhisk/dockerskeleton:nightly
+docker pull openwhisk/example:nightly
 docker pull openwhisk/apigateway:0.11.0
 
 cd $ROOTDIR
