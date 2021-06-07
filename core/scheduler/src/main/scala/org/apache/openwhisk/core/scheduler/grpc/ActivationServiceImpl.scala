99fb2c59e264d820d6b95a6319b7882f19aead4d
ningyougang <415622920@qq.com>
2021-06-07 12:51:51 +0800

    Implement ActivationClientProxy (#5119)
    ActivationClientProxy actor fetches activationMessage from scheudler side
    using akka grpc, and forwards the activationMessage to its parent actor
    FunctionPullContainerProxy actor.

--- core/scheduler/src/main/scala/org/apache/openwhisk/core/scheduler/grpc/ActivationServiceImpl.scala
+++ core/scheduler/src/main/scala/org/apache/openwhisk/core/scheduler/grpc/ActivationServiceImpl.scala
@@ -20,7 +20,8 @@ package org.apache.openwhisk.core.scheduler.grpc
 import akka.actor.ActorSystem
 import akka.pattern.ask
 import akka.util.Timeout
-import org.apache.openwhisk.common.Logging
+import org.apache.openwhisk.common.{Logging, TransactionId}
+import org.apache.openwhisk.core.WarmUp
 import org.apache.openwhisk.core.connector.{ActivationMessage, Message}
 import org.apache.openwhisk.core.entity.{DocRevision, FullyQualifiedEntityName}
 import org.apache.openwhisk.core.scheduler.queue._
@@ -51,7 +52,7 @@ class ActivationServiceImpl()(implicit actorSystem: ActorSystem, logging: Loggin
               this,
               s"Enqueue activation message to reschedule ${request.invocationNamespace} ${request.fqn} ${request.rev}")
             queueValue.queue ? res._3
-            Future.successful(RescheduleResponse(isRescheduled = true))
+            Future.successful(RescheduleResponse(true))
           case None =>
             logging.error(this, s"Queue not found for ${request.invocationNamespace} ${request.fqn} ${request.rev}")
             Future.successful(RescheduleResponse())
@@ -65,31 +66,43 @@ class ActivationServiceImpl()(implicit actorSystem: ActorSystem, logging: Loggin
       fqn <- FullyQualifiedEntityName.parse(request.fqn)
       rev <- DocRevision.parse(request.rev)
     } yield (fqn, rev)).flatMap(Future.fromTry) flatMap { res =>
-      val key = res._1.toDocId.asDocInfo(res._2)
-      QueuePool.get(MemoryQueueKey(request.invocationNamespace, key)) match {
-        case Some(queueValue) =>
-          (queueValue.queue ? GetActivation(
-            res._1,
-            request.containerId,
-            request.warmed,
-            request.lastDuration,
-            request.alive))
-            .mapTo[ActivationResponse]
-            .map { response =>
-              FetchResponse(response.serialize)
-            }
-            .recover {
-              case t: Throwable =>
-                logging.error(this, s"Failed to get message from QueueManager, error: ${t.getMessage}")
-                FetchResponse(ActivationResponse(Left(NoActivationMessage())).serialize)
-            }
-        case None =>
-          if (QueuePool.keys.exists { mkey =>
-                mkey.invocationNamespace == request.invocationNamespace && mkey.docInfo.id == key.id
-              })
-            Future.successful(FetchResponse(ActivationResponse(Left(ActionMismatch())).serialize))
-          else
-            Future.successful(FetchResponse(ActivationResponse(Left(NoMemoryQueue())).serialize))
+      val (fqn, rev) = res
+      if (!WarmUp.isWarmUpAction(fqn)) {
+        val key = fqn.toDocId.asDocInfo(rev)
+        QueuePool.get(MemoryQueueKey(request.invocationNamespace, key)) match {
+          case Some(queueValue) =>
+            implicit val transid = TransactionId.serdes.read(request.transactionId.parseJson)
+            (queueValue.queue ? GetActivation(
+              transid,
+              fqn,
+              request.containerId,
+              request.warmed,
+              request.lastDuration,
+              request.alive))
+              .mapTo[ActivationResponse]
+              .map { response =>
+                FetchResponse(response.serialize)
+              }
+              .recover {
+                case t: Throwable =>
+                  logging.error(
+                    this,
+                    s"Failed to get message from QueueManager container: ${request.containerId}, fqn: ${request.fqn}, rev: ${request.rev}, alive: ${request.alive}, lastDuration: ${request.lastDuration}, error: ${t.getMessage}")
+                  FetchResponse(ActivationResponse(Left(NoActivationMessage())).serialize)
+              }
+          case None =>
+            if (QueuePool.keys.exists { mkey =>
+                  mkey.invocationNamespace == request.invocationNamespace && mkey.docInfo.id == key.id
+                })
+              Future.successful(FetchResponse(ActivationResponse(Left(ActionMismatch())).serialize))
+            else
+              Future.successful(FetchResponse(ActivationResponse(Left(NoMemoryQueue())).serialize))
+        }
+      } else {
+        logging.info(
+          this,
+          s"The ${request.fqn} action is an action used to connect a network level connection. So response no activation")
+        Future.successful(FetchResponse(ActivationResponse(Left(NoActivationMessage())).serialize))
       }
     }
   }
@@ -101,7 +114,8 @@ object ActivationServiceImpl {
     new ActivationServiceImpl()
 }
 
-case class GetActivation(action: FullyQualifiedEntityName,
+case class GetActivation(transactionId: TransactionId,
+                         action: FullyQualifiedEntityName,
                          containerId: String,
                          warmed: Boolean,
                          lastDuration: Option[Long],
