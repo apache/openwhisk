3bdc6cdef55fac7a565a58617eadf6e3afc677d8
Michele Sciabarra <michele@sciabarra.com>
2022-12-08 13:37:01 +0000

    fix java

--- tools/github/waitIfDebug.sh
+++ tools/github/waitIfDebug.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "You have an hour to debug this build."
-echo "remove .github/debug to stop the wait now."
+echo "Remove .github/debug to stop the wait now."
 for i in $(seq 1 60) 
 do if test -e .github/debug 
-      sleep 60
-      echo "ngrok active since $i minutes"
+   then sleep 60 ; echo "$i minutes elapsed"
    fi
 done
