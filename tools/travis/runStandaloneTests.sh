fd6dc66f633734a2a3f5371443522d22e292f616
Dominic Kim <style95@apache.org>
2024-03-25 19:02:46 +0900

    Change the nodejs runtime tag to 1.22.0

--- tools/travis/runStandaloneTests.sh
+++ tools/travis/runStandaloneTests.sh
@@ -45,7 +45,7 @@ export KUBECONFIG="$(kind get kubeconfig-path)"
 kubectl config set-context --current --namespace=default
 
 # This is required because it is timed out to pull the image during the test.
-docker pull openwhisk/action-nodejs-v20:1.20.0
+docker pull openwhisk/action-nodejs-v20:1.22.0
 docker pull openwhisk/dockerskeleton:1.15.0
 docker pull openwhisk/example:1.15.0
 docker pull openwhisk/apigateway:0.11.0
